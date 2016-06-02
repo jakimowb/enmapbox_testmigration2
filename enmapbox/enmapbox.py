114249f30634de54d554eef953e2db2f02e35053
Benjamin Jakimow <no.email>
2016-06-02 10:29:00 +0200

    tbd.

--- enmapbox/enmapbox.py
+++ enmapbox/enmapbox.py
@@ -1,7 +1,7 @@
 
 from PyQt4 import QtCore, QtGui, uic
 
-import six, sys, os
+import six, sys, os, gc
 
 def add_to_sys_path(path):
     if path not in sys.path:
@@ -60,7 +60,140 @@ def getIcon():
     assert os.path.exists(p)
     return p
 
+class CanvasLink(QObject):
+    """
+    Describes a bi-directional link between two QgsMapCanvas
+    """
+    #global list to store all links and avoid circular links
+    LINKSETS = list() #sets of canvases linked by extent
 
+    """
+    if one canvas has changed:
+        set all extents of canvases linked to = in same link group in LINK_EXTENTS
+        set all centers of canvases linked to = in same link group in LINK_ZOOM
+
+        a canvas can be either in a set of LINK_EXTENTS or LINK_CENTERS
+        LINK_CENTERS allows for different zoom levels
+        -> canvases can be linked by center
+        Example: c = linked by center, e = linked by extent
+        A c B
+        B e D
+        = linkset(e : {B,D})
+        = linkset(c : {A,B})
+
+          A B C D
+        A
+        B z
+        C - e
+        D e - z
+
+    """
+    class linkset(set):
+        def __init__(self, linktype):
+            assert linktype in ['extent','center']
+            self.linktype = linktype
+
+        def __add__(self, other):
+            assert isinstance(other, qgis.gui.QgsMapCanvas)
+
+
+    @staticmethod
+    def CreateLink(canvas1, canvas2, linktype='extent', bidirectional=True):
+        assert linktype in ['extent','center']
+        assert isinstance(canvas1, qgis.gui.QgsMapCanvas)
+        assert isinstance(canvas1, qgis.gui.QgsMapCanvas)
+        if canvas1 is canvas2:
+            return None
+
+        register2 = not CanvasLink.containsCanvas(canvas2)
+        register1 = not CanvasLink.containsCanvas(canvas1)
+        #remove canvas1 from existing sets, since it can follow only one other canvas
+        for ls in [ls for ls in CanvasLink.LINKSETS if canvas1 in ls]:
+            ls.remove(canvas1)
+
+
+        ls = [ls for ls in CanvasLink.LINKSETS if ls.linktype==linktype and canvas2 in ls]
+        assert len(ls) <= 1
+        if len(ls) == 1:
+            ls[0].add(canvas1)
+        else:
+            ls = CanvasLink.linkset(linktype)
+            ls.add(canvas1)
+            ls.add(canvas2)
+            CanvasLink.LINKSETS.append(ls)
+
+        # register events
+        if register2:
+            canvas2.extentsChanged.connect(lambda: CanvasLink.setLinks(canvas2))
+        if register1:
+            canvas1.extentsChanged.connect(lambda: CanvasLink.setLinks(canvas1))
+
+    @staticmethod
+    def containsCanvas(canvas):
+        for ls in CanvasLink.LINKSETS:
+            if canvas in ls:
+                return True
+        return False
+
+    @staticmethod
+    def setLinks(canvas):
+        #find linked canvases
+        extents = canvas.extent()
+        center = canvas.center()
+
+        already_changed = set([canvas])
+
+        def applyLinkset(ls, blocksignals=True):
+            if canvas in ls:
+                for c in [c for c in list(ls) if c not in already_changed]:
+                    c.blockSignals(blocksignals)
+                    if ls.linktype == 'extent':
+                        c.setExtent(extents)
+                    elif ls.linktype == 'center':
+                        c.setCenter(center)
+                    else:
+                        raise NotImplementedError()
+                    c.blockSignals(False)
+                    already_changed.add(c)
+
+
+        for ls in [ls for ls in CanvasLink.LINKSETS if ls.linktype == 'extent']:
+            applyLinkset(ls, True)
+
+        for ls in [ls for ls in CanvasLink.LINKSETS if ls.linktype == 'center']:
+            applyLinkset(ls)
+
+        s = ""
+
+    @staticmethod
+    def link_exists(canvas1, canvas2):
+        # check circular reference
+        linked1 = CanvasLink.get_linked_canvases(canvas1)
+        linked2 = CanvasLink.get_linked_canvases(canvas2)
+
+        return canvas2 in linked1 or canvas1 in linked2
+
+    @staticmethod
+    def get_linked_canvases(canvas):
+        linked = set()
+        linked.add(canvas)
+        n = len(linked)
+        changed = True
+        if changed == True:
+            for link in CanvasLink.LINK_SETS:
+                if link.canvas1 in linked:
+                    linked.add(link.canvas2)
+                if link.canvas2 in linked:
+                    linked.add(link.canvas1)
+            changed = len(linked) > n
+        linked.remove(canvas)
+        return linked
+
+    def setExtent(self, c1, c2):
+        pass
+
+    def panCenter(self, c1, c2):
+        pass
 
 class EnMAPBox:
     """Main class that drives the EnMAPBox_GUI and all the magic behind"""
@@ -149,18 +282,36 @@ class TestData():
     pass
 
 
-from pyqtgraph.dockarea.DockArea import DockArea
-from pyqtgraph.dockarea.Dock import Dock, DockLabel
-from pyqtgraph.dockarea.DockDrop import *
-class EnMAPBoxDock(Dock):
+
+class DataSourceRegistry():
+
+    pass
+
+import pyqtgraph.dockarea.DockArea
+class EnMAPBoxDockArea(pyqtgraph.dockarea.DockArea):
+
+    def __init__(self, *args, **kwds):
+        super(EnMAPBoxDockArea, self).__init__(*args, **kwds)
+
+
+
+    def addView(self, enmapboxdock, **kwds):
+        assert isinstance(enmapboxdock, EnMAPBoxDock)
+        return self.addDock(dock=enmapboxdock, **kwds)
+
+
+import pyqtgraph.dockarea.Dock
+class EnMAPBoxDock(pyqtgraph.dockarea.Dock):
     '''
     Handle style sheets etc., basic stuff that differs from pyqtgraph dockarea
     '''
+
     def __init__(self, enmapbox, name='view', closable=True, *args, **kwds):
         super(EnMAPBoxDock, self).__init__(name=name, closable=False, *args, **kwds)
         self.enmapbox = enmapbox
         self.title = name
         self.setStyleSheet('background:#FFF')
+
         #change the enmapbox-like things
         self.topLayout.removeWidget(self.label)
         del self.label
@@ -207,6 +358,8 @@ class EnMAPBoxDock(Dock):
         self.widgetArea.setStyleSheet(self.hStyle)
         self.topLayout.update()
 
+
+
     def _getLabel(self):
         """
         This functions returns the Label that is used to style the Dock
@@ -226,7 +379,7 @@ class EnMAPBoxDockLabel(VerticalLabel):
     sigCloseClicked = QtCore.Signal()
     sigNormalClicked = QtCore.Signal()
 
-    def __init__(self, dock):
+    def __init__(self, dock, allow_floating=True):
         assert isinstance(dock, EnMAPBoxDock)
         self.dim = False
         self.fixedWidth = False
@@ -242,15 +395,17 @@ class EnMAPBoxDockLabel(VerticalLabel):
 
         closeButton = QtGui.QToolButton(self)
         closeButton.clicked.connect(self.sigCloseClicked)
+        closeButton.setToolTip('Close window')
         closeButton.setIcon(QtGui.QApplication.style().standardIcon(QtGui.QStyle.SP_TitleBarCloseButton))
         self.buttons.append(closeButton)
 
-        if True:
-            testButton = QtGui.QToolButton(self)
+        if allow_floating:
+            floatButton = QtGui.QToolButton(self)
             #testButton.clicked.connect(self.sigNormalClicked)
-            testButton.clicked.connect(lambda : self.dock.float())
-            testButton.setIcon(QtGui.QApplication.style().standardIcon(QtGui.QStyle.SP_TitleBarNormalButton))
-            self.buttons.append(testButton)
+            floatButton.setToolTip('Float window')
+            floatButton.clicked.connect(lambda : self.dock.float())
+            floatButton.setIcon(QtGui.QApplication.style().standardIcon(QtGui.QStyle.SP_TitleBarNormalButton))
+            self.buttons.append(floatButton)
 
     def updateStyle(self):
         r = '3px'
@@ -338,34 +493,165 @@ class EnMAPBoxDockLabel(VerticalLabel):
 
         super(EnMAPBoxDockLabel, self).resizeEvent(ev)
 
+
+class CanvasLinkTargetWidget(QFrame):
+
+    LINK_TARGET_WIDGETS = set()
+
+    @staticmethod
+    def ShowMapLinkTargets(canvas1):
+
+        assert isinstance(canvas1, qgis.gui.QgsMapCanvas)
+        CanvasLinkTargetWidget.RemoveMapLinkTargetWidgets(True)
+
+
+        target_canvases = [c for c in gc.get_referrers(qgis.gui.QgsMapCanvas)
+                           if isinstance(c, qgis.gui.QgsMapCanvas) and c is not canvas1]
+
+        for canvas in target_canvases:
+
+            w = CanvasLinkTargetWidget(canvas1, canvas)
+            w.setAutoFillBackground(True)
+            w.show()
+            CanvasLinkTargetWidget.LINK_TARGET_WIDGETS.add(w)
+            canvas.freeze()
+            s = ""
+
+        s = ""
+
+    @staticmethod
+    def linkMaps(maplinkwidget, linktype):
+
+        CanvasLink.CreateLink(maplinkwidget.canvas1, maplinkwidget.canvas2, linktype)
+        CanvasLinkTargetWidget.RemoveMapLinkTargetWidgets()
+
+    @staticmethod
+    def RemoveMapLinkTargetWidgets(processEvents=True):
+        for w in list(CanvasLinkTargetWidget.LINK_TARGET_WIDGETS):
+            CanvasLinkTargetWidget.LINK_TARGET_WIDGETS.remove(w)
+            p = w.parent()
+            w.hide()
+            del(w)
+            p.refresh()
+            p.update()
+
+        if processEvents:
+            qApp.processEvents()
+
+    def __init__(self, canvas1, canvas2):
+        QFrame.__init__(self, parent=canvas2)
+        self.canvas1 = canvas1
+        self.canvas2 = canvas2
+        self.layout = QGridLayout(self)
+        self.setLayout(self.layout)
+
+        self.setCursor(Qt.ArrowCursor)
+
+        ly = QHBoxLayout()
+        self.linkCenter = QtGui.QToolButton(self)
+        self.linkCenter.setToolTip('Link to map center')
+        self.linkCenter.clicked.connect(lambda: CanvasLinkTargetWidget.linkMaps(self, 'center'))
+        self.linkCenter.setAttribute(Qt.WA_PaintOnScreen)
+        self.linkCenter.setIcon(QtGui.QApplication.style().standardIcon(QtGui.QStyle.SP_DirLinkIcon))
+
+        self.linkExtent = QtGui.QToolButton(self)
+        self.linkExtent.setToolTip('Link to map extent')
+        self.linkExtent.setAttribute(Qt.WA_PaintOnScreen)
+        self.linkExtent.clicked.connect(lambda: CanvasLinkTargetWidget.linkMaps(self, 'extent'))
+        self.linkExtent.setIcon(QtGui.QApplication.style().standardIcon(QtGui.QStyle.SP_CommandLink))
+
+        ly.addWidget(self.linkCenter)
+        ly.addWidget(self.linkExtent)
+
+        self.layout.addLayout(ly, 1,1)
+
+        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        #sizePolicy.setHorizontalStretch(0)
+        #sizePolicy.setVerticalStretch(0)
+        #self.setSizePolicy(sizePolicy)
+        #self.resize(100, 50)
+        #self.setAutoFillBackground(True)
+        self.setStyleSheet('background-color:rgba(200, 200, 200, 180)')
+        self.setAttribute(Qt.WA_PaintOnScreen)
+        #self.setBackgroundRole(QPalette.Window)
+
+        s = ""
+        self.updatePosition()
+
+    def updatePosition(self):
+        if hasattr(self.parent(), 'viewport'):
+            parentRect = self.parent().viewport().rect()
+
+        else:
+            parentRect = self.parent().rect()
+
+        if not parentRect:
+            return
+
+        #get map center
+        x = int(parentRect.width() / 2 - self.width() / 2)
+        y = int(parentRect.height() / 2 - self.height() / 2)
+
+        #self.setGeometry(x, y, self.width(), self.height())
+        self.setGeometry(parentRect)
+
+    def setParent(self, parent):
+        self.updatePosition()
+        return super(CanvasLinkTargetWidget, self).setParent(parent)
+
+    def resizeEvent(self, event):
+        super(CanvasLinkTargetWidget, self).resizeEvent(event)
+        self.updatePosition()
+
+    def showEvent(self, event):
+        self.updatePosition()
+        return super(CanvasLinkTargetWidget, self).showEvent(event)
+
+
+    def mousePressEvent(self, ev):
+
+        if ev.button() == Qt.RightButton:
+            #no choice, remove Widgets
+            CanvasLinkTargetWidget.RemoveMapLinkTargetWidgets(True)
+            ev.accept()
+
+
 class EnMAPBoxMapDockLabel(EnMAPBoxDockLabel):
 
     def __init__(self, *args, **kwds):
 
         super(EnMAPBoxMapDockLabel, self).__init__(*args, **kwds)
 
-        linkButton = QtGui.QToolButton(self)
-        # testButton.clicked.connect(self.sigNormalClicked)
+        self.linkMap = QtGui.QToolButton(self)
+        self.linkMap.setToolTip('Link with other map')
+        #linkExtent.clicked.connect(lambda: self.dock.linkWithMapDock())
+        self.linkMap.setIcon(QtGui.QApplication.style().standardIcon(QtGui.QStyle.SP_CommandLink))
 
-        linkButton.clicked.connect(lambda: self.dock.linkWithMapDock())
-        linkButton.setIcon(QtGui.QApplication.style().standardIcon(QtGui.QStyle.SP_CommandLink))
-        self.buttons.append(linkButton)
+        self.buttons.append(self.linkMap)
 
 
 class EnMAPBoxMapDock(EnMAPBoxDock):
     """
     A dock to visualize geodata that can be mapped
     """
+    @staticmethod
+    def get_canvases():
+        return [c for c in gc.get_referrers(qgis.gui.QgsMapCanvas) if isinstance(c, qgis.gui.QgsMapCanvas)]
+
     def __init__(self, *args, **kwds):
         initSrc = kwds.pop('initSrc', None)
 
 
+
         super(EnMAPBoxMapDock, self).__init__(*args, **kwds)
 
+        #self.actionLinkExtent = QAction(QtGui.QApplication.style().standardIcon(QtGui.QStyle.SP_CommandLink), 'Link to map extent', self)
+        #self.actionLinkCenter = QAction(QtGui.QApplication.style().standardIcon(QtGui.QStyle.SP_CommandLink), 'Linkt to map center', self)
+        #self.label.buttons.append(self.actionLinkCenter.getButton())
         self.canvas = qgis.gui.QgsMapCanvas(self)
         settings = QSettings()
         assert isinstance(self.canvas, qgis.gui.QgsMapCanvas)
-        self.canvas.setCanvasColor(Qt.green)
+        self.canvas.setCanvasColor(Qt.black)
         self.canvas.enableAntiAliasing(settings.value('/qgis/enable_anti_aliasing', False, type=bool))
         self.canvas.useImageToRender(settings.value('/qgis/use_image_to_render', False, type=bool))
         self.layout.addWidget(self.canvas)
@@ -389,7 +675,8 @@ class EnMAPBoxMapDock(EnMAPBoxDock):
         self.toolZoomOut.setAction(g.actionZoomOut)
         self.toolZoomOut.action().triggered.connect(lambda: self.canvas.setMapTool(self.toolZoomOut))
 
-        #todo: add pixel inspect
+
+        self.label.linkMap.clicked.connect(lambda:CanvasLinkTargetWidget.ShowMapLinkTargets(self.canvas))
 
         #set default map tool
         self.canvas.setMapTool(self.toolPan)
@@ -399,14 +686,30 @@ class EnMAPBoxMapDock(EnMAPBoxDock):
         if initSrc:
             self.addLayer(initSrc)
 
+    def test(self):
+        print('START LINKING')
+
+        w = CanvasLinkTargetWidget(self.enmapbox.gui)
+        s = ""
+
     def _getLabel(self):
         return EnMAPBoxMapDockLabel(self)
 
-    def linkWithMapDock(self, mapDock):
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+                s = ""
+        else:
+            super(EnMAPBoxMapDock, self).mousePressEvent(event)
+
+    def linkWithMapDock(self, mapDock, linktype='extent'):
         assert isinstance(mapDock, EnMAPBoxMapDock)
+        self.linkWithCanvas(mapDock.canvas, linktype=linktype)
 
 
+    def linkWithCanvas(self, canvas, linktype='extent'):
+        assert isinstance(canvas, qgis.gui.QgsMapCanvas)
 
+        CanvasLink.CreateLink(canvas, self.canvas, linktype=linktype)
 
     def addLayer(self, layerSrc):
 
@@ -448,15 +751,7 @@ class EnMAPBoxTextDock(EnMAPBoxDock):
 
 
 
-class EnMAPBoxDockArea(DockArea):
 
-    def __init__(self, *args, **kwds):
-        super(EnMAPBoxDockArea, self).__init__(*args, **kwds)
-
-
-    def addView(self, enmapboxdock, **kwds):
-        assert isinstance(enmapboxdock, EnMAPBoxDock)
-        self.addDock(dock=enmapboxdock, **kwds)
 
 
 
@@ -481,18 +776,26 @@ if __name__ == '__main__':
     qgsApp.initQgis()
 
 
-#    w = QMainWindow()
-#    w.setWindowTitle('QgsMapCanvas Example')
-#    w.show()
+    if False:
+        w = QMainWindow()
+        w.setWindowTitle('QgsMapCanvas Example')
+
+        w.setLayout(QtGui.QGridLayout())
+        w.layout().addWidget(CanvasLinkTargetWidget(None, None, parent=w))
+        w.show()
+
+
    # EB = EnMAPBox(w)
     EB = EnMAPBox(None)
     EB.dockarea.addView(EnMAPBoxDock(EB, name='view1 Default'))
-    EB.dockarea.addView(EnMAPBoxMapDock(EB, name='view2: a map', initSrc=TestData.Image()))
+    md1 = EB.dockarea.addView(EnMAPBoxMapDock(EB, name='view2: a map', initSrc=TestData.Image()))
     EB.dockarea.addView(EnMAPBoxTextDock(EB,
                                          name='view3: a text/info window',
                                          html='Lore <i>ipsum</i> tralalla<br/> '
                                               '<a href="http://www.enmap.org">www.enmap.org</a>'))
-    EB.dockarea.addView(EnMAPBoxMapDock(EB, name='view4: another map', initSrc=TestData.Image()))
+    md2 = EB.dockarea.addView(EnMAPBoxMapDock(EB, name='view4: another map', initSrc=TestData.Image()))
+
+    #md1.linkWithMapDock(md2, linktype='center')
     #EB.show()
     EB.run()
 
