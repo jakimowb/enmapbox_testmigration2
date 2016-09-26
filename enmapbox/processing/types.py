e162d837567b340e2742566e812fb1f727898e2f
mahe <matthias.held@geo.hu-berlin.de>
2016-09-26 15:21:38 +0200

    minor report changes #2

--- enmapbox/processing/types.py
+++ enmapbox/processing/types.py
@@ -232,33 +232,31 @@ class ImageStatistics(Type):
         report.append(ReportMonospace(self.image.filename))
         report.append(ReportHeading('Basic Statistics'))
 
-        colHeaders = [['#Band','Band Name','n total', 'n ignored','n used', 'min', 'max']]
+        colHeaders = [['Band','Band Name','n total', 'n ignored','n used', 'min', 'max']]
+        bandColumn = self.getStatistic('band')
         #convert band number list to string
-        bandColumn = [str(i) for i in self.getStatistic('band')]
+        #bandColumn = [str(i) for i in self.getStatistic('band')]
         #change strings of band column
-        bandColumn = ['#'+ s for s in bandColumn]
+        #bandColumn = ['#'+ s for s in bandColumn]
         bandNames = self.image.meta.getBandNames()
         #bandColumn = [a+b.lstrip() for a,b in zip(bandColumn,bandNames)]
 
         if self.image.meta.getMetadataItem('wavelength') != None:
             wl = numpy.round(numpy.array(self.image.meta.getMetadataItem('wavelength')).astype(float),3).astype(str)
-            bandNames = [a+' ('+b.lstrip()+')' for a,b in zip(bandNames,wl)]
-            colHeaders[0][1]=colHeaders[0][1]+', wl'
+            bandNames = [a+' ('+b.lstrip() for a,b in zip(bandNames,wl)]
+            #colHeaders[0][1]=colHeaders[0][1]+', wl'
         if self.image.meta.getMetadataItem('wavelength units') != None:
             wlUnits = self.image.meta.getMetadataItem('wavelength units')
             if wlUnits == 'micrometers': wlUnits = '&mu;m'
             if wlUnits == 'nanometers': wlUnits = ' nm'
             bandNames = [s + ' ' + wlUnits for s in bandNames]
-
+        if self.image.meta.getMetadataItem('wavelength') != None:
+            bandNames = [s + ')' for s in bandNames]
         rowSpans = None
         colSpans = [[1,1,1,1,1,1,1]]
         rowHeaders = None
-        # todo where does the max = 6 come from? there are only 5 classes
-
-
         data = numpy.transpose([bandColumn,bandNames,self.getStatistic('count'),numpy.array(self.getStatistic('count'))-numpy.array(self.getStatistic('countValid')),self.getStatistic('countValid'),self.getStatistic('min'),self.getStatistic('max')])
         report.append(ReportTable(data, '', colHeaders, rowHeaders, colSpans, rowSpans, attribs_align='left'))
-
 
         #report.append(ReportParagraph('bands = ' + str(self.getStatistic('band'))))
         #report.append(ReportParagraph('mins = ' + str(self.getStatistic('min'))))
@@ -276,7 +274,6 @@ class ImageStatistics(Type):
 
         #for i in range(0,self.bandStatistics.__len__()):
         for i in range(0,1):
-        # todo: test for alternating image - classification - image - classification - stacks
         # todo order tables and plots, provide headlines for both cases
             report.append(ReportHeading('Band ' + str(i+1),sub=1))
 
@@ -299,7 +296,7 @@ class ImageStatistics(Type):
                    ))
             report.append(ReportTable(data, '', rowHeaders=rowHeaders))
 
-            if self.bandStatistics[0].classification == True:
+            if self.image.meta.isClassification():
                 colSpans = [[3],[1,1,1]]
                 colHeaders = [['Classification scheme'],['DN','class name', 'counts']]
                 data = numpy.transpose([range(0,numpy.array(self.image.meta.getMetadataItem('classes')).astype(int))
@@ -307,7 +304,6 @@ class ImageStatistics(Type):
                         ,numpy.hstack((self.getStatistic('countNoDataValue')[i],self.getStatistic('hist')[i].astype(int)))])
                 report.append(ReportTable(data, '', colHeaders=colHeaders, colSpans=colSpans))
 
-            # todo can we assume there is class lookup?
                 colorArray = numpy.array(self.image.meta.getMetadataItem('class lookup')[3:]).astype(float)/255
                 colorTuples = zip(colorArray[0::3],colorArray[1::3],colorArray[2::3])
                 fig, ax = plt.subplots(facecolor='white')
@@ -318,7 +314,7 @@ class ImageStatistics(Type):
                 xticks = ax.xaxis.get_major_ticks()
                 xticks[0].label1.set_visible(False)
                 ax.set_ylabel('counts')
-                ax.set_title('Pixel per class',y=1.05)
+                ax.set_title('Pixel per class', y=1.05)
                 ax.set_xticklabels((self.image.meta.getMetadataItem('class names')))
                 plt.xticks(rotation=35)
                 plt.tight_layout()
@@ -338,7 +334,6 @@ class ImageStatistics(Type):
                 report.append(ReportPlot(fig, ''))
                 plt.close()
 
-                # todo change some columns to integer, note: does not work?!
                 colHeaders = [['# bin','binStart','binEnd','count','cum. counts','prob. density','cum. distribution']]
                 data = numpy.transpose([numpy.array(range(0,self.getStatistic('hist')[i].__len__())).astype(int)
                                        ,numpy.round(self.getStatistic('bin_edges')[i][0:-1],2)
@@ -349,10 +344,6 @@ class ImageStatistics(Type):
                                        ,numpy.round(numpy.cumsum(self.getStatistic('hist')[i]/self.getStatistic('countValid')[i]),2)
                                    ])
                 report.append(ReportTable(data, '', colHeaders=colHeaders))
-
-
-
-
 
         #report.append(ReportParagraph('hists = ' + str(self.getStatistic('hist'))))
         #report.append(ReportParagraph('bin_edges = ' + str(self.getStatistic('bin_edges'))))
@@ -1341,11 +1332,17 @@ class ClassificationPerformance(Type):
 
         if self.adjusted:
             report.append(ReportHeading('Stratification'))
-            report.append(ReportParagraph('strataClasses: ' + str(self.strataClasses)))
-            report.append(ReportParagraph('strataClassNames: ' + str(self.strataClassNames)))
-            report.append(ReportParagraph('strataSizes: ' + str(self.strataSizes)))
-            report.append(ReportParagraph('strataSampleSizes: ' + str(self.strataSampleSizes)))
-            report.append(ReportParagraph('strataWeights: ' + str(self.strataWeights)))
+
+            colHeaders = [['DN','Stratum', 'Stratum Size', 'Stratum Sample Size', 'Adjustment Weight']]
+            colSpans = [[1,1,1,1,1]]
+            data = numpy.transpose([numpy.array(range(0, self.strataClasses))+1, self.strataClassNames, self.strataSizes, self.strataSampleSizes, numpy.round(self.strataWeights,2) ])
+            report.append(ReportTable(data, '', colHeaders=colHeaders, colSpans=colSpans))
+
+            #report.append(ReportParagraph('strataClasses: ' + str(self.strataClasses)))
+            #report.append(ReportParagraph('strataClassNames: ' + str(self.strataClassNames)))
+            #report.append(ReportParagraph('strataSizes: ' + str(self.strataSizes)))
+            #report.append(ReportParagraph('strataSampleSizes: ' + str(self.strataSampleSizes)))
+            #report.append(ReportParagraph('strataWeights: ' + str(self.strataWeights)))
 
         report.append(ReportHeading('Classification Label Overview'))
         colHeaders = None
@@ -1353,7 +1350,7 @@ class ClassificationPerformance(Type):
         colSpans = [[1,1,1,1,1]]
         rowHeaders = [['','Class Names'],['Class ID','Reference', 'Prediction']]
         data = [numpy.hstack((0,self.classLabels)),self.sample.labelsSample.dataSample.meta.getMetadataItem('class names'),self.sample.labelsSample.dataSample.meta.getMetadataItem('class names')]
-        report.append(ReportTable(data, '', colHeaders, rowHeaders, colSpans, rowSpans))
+        report.append(ReportTable(data, '', colHeaders=colHeaders, rowHeaders=rowHeaders, colSpans=colSpans, rowSpans=rowSpans))
 
         # Confusion Matrix Table
         report.append(ReportHeading('Confusion Matrix'))
@@ -1529,12 +1526,12 @@ class RegressionPerformance(Type):
         report.append(ReportParagraph('Prediction: ' + self.sample.featureSample.filename))
 
         report.append(ReportHeading('Performance Measures'))
-        # todo align Table cells to left
+
         colHeaders = [['Metric','Abbr.','Value']]
         data = numpy.transpose([['Number of sample pairs','Squared pearson correlation','Mean absolute error','Median absolute error','Median squared error']
-                               ,['n','r^2','MAE','MAD???','MSE']
+                               ,['n','r^2','MAE','MedianAE','MSE']
                                ,[self.n,numpy.round(numpy.array(self.explained_variance_score).astype(float),2),numpy.round(numpy.array(self.mean_absolute_error).astype(float),2),numpy.round(numpy.array(self.median_absolute_error).astype(float),2),numpy.round(numpy.array(self.mean_squared_error).astype(float),2)]])
-        report.append(ReportTable(data, 'Performance Metrics', colHeaders=colHeaders))
+        report.append(ReportTable(data, 'Performance Metrics', colHeaders=colHeaders, attribs_align='left'))
 
         report.append(ReportHeading('Results'))
         # Estimate the point density
@@ -1557,23 +1554,27 @@ class RegressionPerformance(Type):
         ax0.xaxis.set_ticks_position('bottom')
         ax0.yaxis.set_ticks_position('left')
         # plot only every second tick, starting with the second
-        for label in ax0.get_yticklabels()[1::2]: label.set_visible(False)
+        #for label in ax0.get_yticklabels()[1::2]: label.set_visible(False)
+        #plot only first and last ticklabel
+        for label in ax0.get_yticklabels()[1:-1]: label.set_visible(False)
 
         ax1 = plt.subplot(gs[1,1])
         ax1.hist(self.prediction, orientation='horizontal',bins=100, edgecolor='None')
         plt.tick_params(which = 'both', direction = 'out', length=10, pad=10)
-        plt.ylim([numpy.min(self.prediction),numpy.max(self.prediction)])
+        plt.ylim([numpy.min(self.reference),numpy.max(self.reference)])
         # hide ticks and ticklabels
         ax1.set_yticklabels([])
         ax1.yaxis.set_ticks_position('left')
         ax1.xaxis.set_ticks_position('bottom')
         # plot only every second tick, starting with the second
-        for label in ax1.get_xticklabels()[1::2]: label.set_visible(False)
+        #for label in ax1.get_xticklabels()[1::2]: label.set_visible(False)
+        #plot only first and last ticklabel
+        for label in ax1.get_xticklabels()[1:-1]: label.set_visible(False)
 
         ax2 = plt.subplot(gs[1,0])
         ax2.scatter(self.reference,self.prediction, s=10, edgecolor='', color='navy')
         plt.xlim([numpy.min(self.reference),numpy.max(self.reference)])
-        plt.ylim([numpy.min(self.prediction),numpy.max(self.prediction)])
+        plt.ylim([numpy.min(self.reference),numpy.max(self.reference)])
         plt.tick_params(which = 'both', direction = 'out')
         plt.xlabel('Reference')
         plt.ylabel('Estimation')
@@ -1629,11 +1630,14 @@ class ClusteringPerformance(Type):
         report.append(ReportHeading('Performance Measures'))
 
         report.append(ReportParagraph('n = ' + str(self.n)))
-        rowHeaders = [['Adjusted Mutual Information','Adjusted Rand index','Completeness Score']]
-        data = [[numpy.round(self.adjusted_mutual_info_score,3),numpy.round(self.adjusted_rand_score,3),numpy.round(self.completeness_score,3)]]
-        report.append(ReportTable(data, '', rowHeaders))
+        rowHeaders = [['Adjusted Mutual Information','Adjusted Rand Score','Completeness Score']]
+        data = numpy.transpose([[numpy.round(self.adjusted_mutual_info_score,3),numpy.round(self.adjusted_rand_score,3),numpy.round(self.completeness_score,3)]])
+        report.append(ReportTable(data, '', rowHeaders=rowHeaders))
         report.append(ReportHeading('Scikit-Learn Documentation'))
-        report.append(ReportHyperlink('http://scikit-learn.org/stable/modules/clustering.html#clustering-performance-evaluation', 'Clustering Performance Evaluation'))
+        report.append(ReportHyperlink('http://scikit-learn.org/stable/modules/clustering.html#clustering-performance-evaluation', 'Clustering Performance Evaluation Overview'))
+        report.append(ReportHyperlink('http://scikit-learn.org/stable/modules/generated/sklearn.metrics.adjusted_mutual_info_score.html#sklearn.metrics.adjusted_mutual_info_score', 'Adjusted Mutual Information'))
+        report.append(ReportHyperlink('http://scikit-learn.org/stable/modules/generated/sklearn.metrics.adjusted_rand_score.html#sklearn.metrics.adjusted_rand_score', 'Adjusted Rand Score'))
+        report.append(ReportHyperlink('http://scikit-learn.org/stable/modules/generated/sklearn.metrics.completeness_score.html#sklearn.metrics.completeness_score', 'Completeness Score'))
 
         return report
 
