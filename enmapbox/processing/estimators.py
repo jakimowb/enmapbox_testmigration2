ffe4ef124e05acacc8da8595d03032646e8abf4b
mahe <matthias.held@geo.hu-berlin.de>
2016-10-10 10:43:09 +0200

    robust scaler report

--- enmapbox/processing/estimators.py
+++ enmapbox/processing/estimators.py
@@ -556,7 +556,6 @@ class Transformers(Estimators):
 
 
         def reportDetails(self):
-            # todo report
             # anything to report?
             ica = self.finalEstimator()
             report = Report('')
@@ -589,13 +588,14 @@ class Transformers(Estimators):
         def reportDetails(self):
 
             scaler = self.finalEstimator()
-            # todo report
+
             report = Report('')
             report.append(ReportHeading('Information'))
             bandNames = self.sample.featureSample.dataSample.meta.getBandNames()
-            #table = Table([['<b>Median</b>']+list(scaler.center_),
-                      #     ['<b>Interquartile Range</b>']+list(scaler.scale_)], header_row=['']+bandNames)
-            report.append(ReportTable(data))
+            data = numpy.transpose([bandNames,scaler.center_,scaler.scale_])
+            colHeaders = [['Band Name','Median', 'Interquartile Range']]
+            report.append(ReportTable(data, colHeaders=colHeaders))
+
             return report
 
 PCA = Transformers.PCA
