b34ab30b56e9fd7aaf64075346d1492475e31c8a
janzandr <andreas.rabe@geo.hu-berlin.de>
2016-10-14 14:59:35 +0200

    changed parameters

--- lamos/usecase/gms_beta_usgs_import.py
+++ lamos/usecase/gms_beta_usgs_import.py
@@ -4,11 +4,12 @@ sys.path.append(r'C:\Work\source\site-packages')
 
 
 import os
+import argparse
 from lamos.processing.types import MGRSTilingScheme, WRS2Footprint, MGRSFootprint, MGRSArchive
 from lamos.operators.landsat_x import LandsatXComposer
 from hub.timing import tic, toc
 
-def run(infolder, outfolder, tmpfolder):
+def run(infolder, outfolder, tmpfolder, processes):
 
     # path to shapefiles with WRS2 and MGRS Geometries
     MGRSFootprint.shpRoot = r'C:\Work\data\gms\gis\MGRS_100km_1MIL_Files'
@@ -29,24 +30,25 @@ def run(infolder, outfolder, tmpfolder):
 
     # create sensor x products
     composer = LandsatXComposer()
-    composer.composeWRS2Archive(infolder=folder1, outfolder=folder2, footprints=wrs2Footprints, processes=10)
+    composer.composeWRS2Archive(infolder=folder1, outfolder=folder2, footprints=wrs2Footprints, processes=processes)
 
     # cut sensor x products
     tilingScheme = MGRSTilingScheme(pixelSize=30)
-    tilingScheme.tileWRS2Archive(infolder=folder2, outfolder=folder3, buffer=300, wrs2Footprints=wrs2Footprints, mgrsFootprints=mgrsFootprints, processes=10)
+    tilingScheme.tileWRS2Archive(infolder=folder2, outfolder=folder3, buffer=300, wrs2Footprints=wrs2Footprints, mgrsFootprints=mgrsFootprints, processes=processes)
 
     # save as ENVI
-    MGRSArchive(folder3).saveAsGTiff(folder4, processes=10)
+    MGRSArchive(folder3).saveAsENVI(folder4, processes=processes)
 
 if __name__ == '__main__':
 
     try:
-        infolder, outfolder, tmpfolder = sys.argv
+        infolder, outfolder, tmpfolder, processes = sys.argv
     except:
         infolder = r'C:\Work\data\gms\landsat'
         outfolder = r'C:\Work\data\gms\landsatXMGRS_ENVI'
         tmpfolder = r'C:\Work\data\gms\tmp'
+        processes=10
 
     tic()
-    run(infolder=infolder, outfolder=outfolder, tmpfolder=tmpfolder)
+    run(infolder=infolder, outfolder=outfolder, tmpfolder=tmpfolder, processes=processes)
     toc()
