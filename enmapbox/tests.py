c8c090d9e2c8cf0a17ebd17e3d3d99a592cc7091
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2016-11-10 18:07:02 +0100

    set QGIS_DEBUG to show QGS specific debug printout. however, it does not work

--- enmapbox/tests.py
+++ enmapbox/tests.py
@@ -1,3 +1,14 @@
 if __name__ == '__main__':
+    import enmapbox
+    enmapbox.DEBUG = True
     import enmapbox.main
     enmapbox.main.moduletest()
+
+import sys, os
+
+os.environ['QGIS_DEBUG'] = '1'
+
+def writelogmessage(message, tag, level):
+    print('{}({}): {}'.format( tag, level, message ) )
+from qgis.core import QgsMessageLog
+QgsMessageLog.instance().messageReceived.connect( writelogmessage)
