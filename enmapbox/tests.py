c4818e664011f3c34b8353adccdf21231a5f33b3
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2016-11-11 10:43:13 +0100

    re-structures test

--- enmapbox/tests.py
+++ enmapbox/tests.py
@@ -1,14 +1,20 @@
-if __name__ == '__main__':
-    import enmapbox
-    enmapbox.DEBUG = True
-    import enmapbox.main
-    enmapbox.main.moduletest()
-
 import sys, os
 
-os.environ['QGIS_DEBUG'] = '1'
 
 def writelogmessage(message, tag, level):
     print('{}({}): {}'.format( tag, level, message ) )
 from qgis.core import QgsMessageLog
 QgsMessageLog.instance().messageReceived.connect( writelogmessage)
+
+
+def test_GUI():
+    os.environ['QGIS_DEBUG'] = '1'
+    import enmapbox
+    enmapbox.DEBUG = True
+    import enmapbox.main
+    enmapbox.main.moduletest()
+
+
+
+if __name__ == '__main__':
+    if True: test_GUI()
