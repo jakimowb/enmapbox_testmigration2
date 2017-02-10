d6a40aa6aade3fca4512acdacf2274142fe4768c
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2017-02-10 16:08:53 +0100

    file_search

--- enmapbox/__init__.py
+++ enmapbox/__init__.py
@@ -5,7 +5,7 @@ __all__ = ['enmapbox', 'datasources','gui']
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
 
-import os
+import os, fnmatch
 
 jp = os.path.join
 
@@ -20,6 +20,23 @@ import six
 def dprint(text, file=None):
     if DEBUG:
         six._print('DEBUG::{}'.format(text), file=file)
+
+def file_search(rootdir, wildcard, recursive=False, ignoreCase=False):
+    assert rootdir is not None
+    if not os.path.isdir(rootdir):
+        six.print_("Path is not a directory:{}".format(rootdir), file=sys.stderr)
+
+    results = []
+
+    for root, dirs, files in os.walk(rootdir):
+        for file in files:
+            if (ignoreCase and fnmatch.fnmatch(file.lower(), wildcard.lower())) \
+                    or fnmatch.fnmatch(file, wildcard):
+                results.append(os.path.join(root, file))
+        if not recursive:
+            break
+            pass
+    return results
 
 
 #DIR = os.path.dirname(__file__)
