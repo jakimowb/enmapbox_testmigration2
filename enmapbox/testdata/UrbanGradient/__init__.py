a3b1c46835646c47c80de0411c1e1eaf2dc38756
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2017-02-14 17:22:49 +0100

    Testdata set basenames as module attributes

--- enmapbox/testdata/UrbanGradient/__init__.py
+++ enmapbox/testdata/UrbanGradient/__init__.py
@@ -3,8 +3,8 @@ import os, sys, re
 from enmapbox import jp, file_search
 
 # make testfile locations available as attributes
-for file in  file_search(os.path.dirname(__file__), 'AF_*.bsq') + \
-             file_search(os.path.dirname(__file__), 'AF_*.bip'):
+for file in  file_search(os.path.dirname(__file__), '*.bsq') + \
+             file_search(os.path.dirname(__file__), '*.shp'):
     bn, ext = os.path.splitext(os.path.basename(file))
     setattr(sys.modules[__name__], bn, file)
 
