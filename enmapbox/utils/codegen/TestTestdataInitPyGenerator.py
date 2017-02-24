624d344b6842689ecfe5802579b4f17db8a357a0
janzandr <andreas.rabe@geo.hu-berlin.de>
2017-02-24 10:54:50 +0100

    small unit test fix

--- enmapbox/utils/codegen/TestTestdataInitPyGenerator.py
+++ enmapbox/utils/codegen/TestTestdataInitPyGenerator.py
@@ -10,13 +10,13 @@ CODE = \
 import os
 
 root = os.path.dirname(__file__)
-HymapBerlinA_image = os.path.join(root, "HymapBerlinA_image")
-HymapBerlinA_imagesample = os.path.join(root, "HymapBerlinA_imagesample")
-HymapBerlinA_mask = os.path.join(root, "HymapBerlinA_mask")
-HymapBerlinA_test = os.path.join(root, "HymapBerlinA_test")
-HymapBerlinA_train = os.path.join(root, "HymapBerlinA_train")
-HymapBerlinA_trainsample = os.path.join(root, "HymapBerlinA_trainsample")
-HymapBerlinA_truth = os.path.join(root, "HymapBerlinA_truth")
+HymapBerlinA_image = os.path.join(root, "HymapBerlinA_image.img")
+HymapBerlinA_imagesample = os.path.join(root, "HymapBerlinA_imagesample.img")
+HymapBerlinA_mask = os.path.join(root, "HymapBerlinA_mask.img")
+HymapBerlinA_test = os.path.join(root, "HymapBerlinA_test.img")
+HymapBerlinA_train = os.path.join(root, "HymapBerlinA_train.img")
+HymapBerlinA_trainsample = os.path.join(root, "HymapBerlinA_trainsample.img")
+HymapBerlinA_truth = os.path.join(root, "HymapBerlinA_truth.img")
 '''
 
 class TestTestdataInitGenerator(TestCase):
