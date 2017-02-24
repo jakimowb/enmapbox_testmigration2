26771d687317fabe400e11b57511a7359989b839
janzandr <andreas.rabe@geo.hu-berlin.de>
2017-02-24 19:21:36 +0100

    minor edit

--- enmapbox/utils/environment/DirectoryLookup.py
+++ enmapbox/utils/environment/DirectoryLookup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 class DirectoryLookup(object):
 
-    repository = dirname(dirname(__file__))
+    repository = join(dirname(__file__), *['..']*3)
     site_packages = join(repository, 'site-packages')
     site_packages_os_specific = join(site_packages, os.sys.platform)
     testdata = join(repository, 'testdata')
