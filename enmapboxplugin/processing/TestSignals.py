04c995df941b2091a581011ecbf3875b44308ffe
janzandr <andreas.rabe@geo.hu-berlin.de>
2017-02-28 17:58:26 +0100

    modified Signals

--- enmapboxplugin/processing/TestSignals.py
+++ enmapboxplugin/processing/TestSignals.py
@@ -1,19 +1,7 @@
 from unittest import TestCase
-
-IMAGE_RECEIVED = None
+from Signals import Signals
 
 class TestSignals(TestCase):
 
-    def test_connectDummyHandlerAndEmitSignal(self):
-
-        from Signals import Signals
-
-        def handleImageCreated(filename):
-            global IMAGE_RECEIVED
-            IMAGE_RECEIVED = True
-
-        signals = Signals()
-        signals.imageCreated.connect(handleImageCreated)
-        signals.imageCreated.emit('c:\image.img')
-
-        self.assertTrue(IMAGE_RECEIVED)
+    def test_emitURL(self):
+        Signals.emitHTMLCreated(r'https://google.de')
