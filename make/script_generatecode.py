d4f51af5bdaedc51c84801f5e6d241b733e870ea
janzandr <andreas.rabe@geo.hu-berlin.de>
2017-03-05 08:32:24 +0100

    added testcase for SignalsManager

--- make/script_generatecode.py
+++ make/script_generatecode.py
@@ -1,10 +1,10 @@
 from os.path import join
-from enmapbox import DIR_TESTDATA
+from enmapbox.utils.environment.DirectoryLookup import DirectoryLookup
 from enmapbox.utils.codegen.TestdataInitPyGenerator import TestdataInitPyGenerator
 
 def generate_testdata_initpy():
-    for dir in ['HymapBerlinA', 'HymapBerlinB', 'AlpineForeland', 'UrbanGradient']:
-        root = join(DIR_TESTDATA, dir)
+    for name in ['HymapBerlinA', 'HymapBerlinB', 'UrbanGradient']:
+        root = join(DirectoryLookup.testdata, name)
         print('generated {initpy}'.format(initpy=join(root, '__init__.py')))
         TestdataInitPyGenerator().generate(root=root)
 
