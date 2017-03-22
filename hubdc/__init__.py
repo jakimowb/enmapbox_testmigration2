fecc4a5bdbd9f76f4410e7a561d57913c5fc2682
janzandr <andreas.rabe@geo.hu-berlin.de>
2017-03-22 16:53:49 +0100

    added projectionFromEPSG util function; increased version number

--- hubdc/__init__.py
+++ hubdc/__init__.py
@@ -1,6 +1,7 @@
-__version__ = '0.1.0'
-from rios.imageio import NumpyTypeToGDALType
 from hubdc.model.Cube import Cube
 from hubdc.model.Dataset import Dataset
 from hubdc.model.DatasetList import DatasetList
 from hubdc.model import Open, OpenDSL
+
+
+__version__ = '0.2.0'
