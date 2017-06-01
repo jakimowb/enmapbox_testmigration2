0c394bb7c6614d392f7b4cc6ee18cc1a0c4d7da7
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2017-06-01 10:34:57 +0200

    added app __init__.py

---
+++ enmapbox/apps/exampleapp/__init__.py
@@ -0,0 +1,53 @@
+import os
+
+from PyQt4.QtGui import QIcon
+from enmapbox.gui.applications import EnMAPBoxApplication
+from ExampleApp import *
+APP_DIR = os.path.dirname(__file__)
+
+class MyEnMAPBoxApp(EnMAPBoxApplication):
+
+    def __init__(self, enmapBox, parent=None):
+        super(MyEnMAPBoxApp, self).__init__(enmapBox,parent=parent)
+        self.name = 'My EnMAPBox App'
+        self.version = 'Version 42'
+        self.licence = 'BSD-3'
+        s = ""
+
+    def icon(self):
+        pathIcon = os.path.join(APP_DIR, 'icon.png')
+        return QIcon(pathIcon)
+
+    def menu(self, appMenu):
+        """
+        Specify menu, submenus and actions
+        :return:
+        """
+        menu = QMenu(self.name, appMenu)
+        menu.setIcon(self.icon())
+
+        #add QAction that starts your GUI
+        a = menu.addAction('Show My GUI')
+        a.triggered.connect(lambda : self.startExampleGUI())
+
+        #add a submenu
+        subMenu = menu.addMenu('Submenu')
+        #add QAction to run another process
+        a = subMenu.addAction('Start Process')
+        a.triggered.connect(lambda : self.startExampleProcess('My App Action triggered'))
+
+        return menu
+
+    def geoAlgorithms(self):
+        return [MyAppGeoAlgorithm()]
+
+    def startExampleGUI(self):
+        ui = MyAppUserInterface(self.enmapbox.ui)
+        ui.setModal(False) #True = will block all other widget
+        ui.show()
+
+    def startExampleProcess(self, text):
+        print('print something:')
+        print(text)
+        print('exampleProcess done')
+
