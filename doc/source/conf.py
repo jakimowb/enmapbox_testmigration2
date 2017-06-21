51ca908a74db12d415b773484adfa2aa42826246
janzandr <andreas.rabe@geo.hu-berlin.de>
2017-06-21 11:30:12 +0200

    test rtd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,6 +13,7 @@
 # serve to show the default.
 
 # Set up 'mock' modules, needed to build docs if numpy, gdal etc., aren't installed
+import os
 import sys
 import mock
 
@@ -25,8 +26,8 @@ for mod_name in MOCK_MODULES:
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-#import os
-#import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, r'C:\Work\source\hub-datacube')
 #sys.path.insert(0, r'C:\Work\source\enmap-box-site-packages')
 #sys.path.insert(0, r'C:\Work\source\astropy')
@@ -95,7 +96,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'classic'
+#html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -174,3 +176,4 @@ texinfo_documents = [
 
 
 
+autodoc_member_order = 'groupwise'
