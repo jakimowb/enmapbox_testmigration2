70f744748a809007b23786d3da5b497f49b5258f
janzandr <andreas.rabe@geo.hu-berlin.de>
2017-07-19 14:36:15 +0200

    increased version

--- hubdc/__init__.py
+++ hubdc/__init__.py
@@ -1,8 +1,5 @@
-#from hubdc.applier import Applier, ApplierOperator
-#from hubdc.model import PixelGrid, Dataset, Layer, Create, CreateFromArray, Open, OpenLayer
-
 from distutils.version import LooseVersion
 
-HUBDC_VERSION = '0.4.1'
+HUBDC_VERSION = '0.5.0'
 HUBDC_VERSION_OBJ = LooseVersion(HUBDC_VERSION)
 __version__ = HUBDC_VERSION
