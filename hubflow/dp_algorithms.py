53a6ece365e5ebb3004124eded797660e0dd20c2
janzandr <andreas.rabe@geo.hu-berlin.de>
2017-07-27 20:19:04 +0200

    added ENVI Speclib import and implemented synthMix

--- hubflow/dp_algorithms.py
+++ hubflow/dp_algorithms.py
@@ -47,3 +47,67 @@ def classifierPredictData(classifier, features, progressBar=None):
     progressBar.setLabelText('predict sample (N={}) with {}'.format(features.shape[1], str(classifier).split()))
     labels = classifier.predict(X=features.T)
     return labels
+
+def classifyNames(names, classNames, indexBase=0):
+    assert set(names).issubset(set(classNames))
+    return [classNames.index(name)+indexBase for name in names]
+
+def _synthMix(features, labels, classes, endmemberLikelihoods, classLikelihoods, n):
+    assert matching2dArrays(features, labels)
+
+    indices = dict()
+    for label in range(1,classes+1):
+        indices[label] = numpy.where(labels==label)[1]
+
+    def randomLabels():
+        return numpy.random.choice(classes, size=len(endmemberLikelihoods) ,p=classLikelihoods, replace=False) + 1
+
+    def randomFractions(size):
+        randomWeights = numpy.atleast_2d(numpy.float32(numpy.random.randint(low=1, high=100, size=size)))
+        return randomWeights/randomWeights.sum()
+
+    def createEndmember(label):
+        nProfiles = numpy.random.choice(len(endmemberLikelihoods), p=endmemberLikelihoods, replace=False) + 1
+        targetIndices = numpy.random.choice(indices[label], size=nProfiles, replace=False)
+        targetProfiles = features[:, targetIndices]
+        return numpy.sum(targetProfiles*randomFractions(size=nProfiles), axis=1)
+
+    def createSyntheticMixture():
+        randLabels = randomLabels()
+        randEndmembers = numpy.array([createEndmember(label) for label in randLabels]).T
+        randFractions = randomFractions(size=len(randLabels))
+        mixedProfile = numpy.sum(randEndmembers*randFractions, axis=1)
+        labelFractions = numpy.zeros(classes, dtype=numpy.float32)
+        labelFractions[randLabels-1] = randFractions
+        return mixedProfile, labelFractions
+
+    mixtures, fractions = zip(*[createSyntheticMixture() for i in range(n)])
+    return numpy.array(mixtures).T, numpy.array(fractions).T
+
+def synthMix(features, labels, classes, mixingComplexities, classLikelihoods, n):
+    assert matching2dArrays(features, labels)
+    assert isinstance(mixingComplexities, dict)
+    assert len(classLikelihoods) == classes
+
+    # cache label indices and setup 0%/100% fractions from class labels
+    indices = dict()
+    zeroOneFractions = numpy.zeros((classes, features.shape[1]), dtype=numpy.float32)
+    for label in range(1,classes+1):
+        indices[label] = numpy.where(labels==label)[1]
+        zeroOneFractions[label-1, indices[label]] = 1.
+
+    # create mixtures
+    mixtures = list()
+    fractions = list()
+    for i in range(n):
+        complexity = numpy.random.choice(mixingComplexities.keys(), p=mixingComplexities.values())
+        drawnLabels = numpy.random.choice(range(1, classes+1), size=complexity, replace=False, p=classLikelihoods)
+        drawnIndices = [numpy.random.choice(indices[label]) for label in drawnLabels]
+        drawnFeatures = features[:, drawnIndices]
+        drawnFractions = zeroOneFractions[:, drawnIndices]
+        randomWeights = numpy.atleast_2d(numpy.float32(numpy.random.randint(low=1, high=100, size=complexity)))
+        randomWeights /= randomWeights.sum()
+        mixtures.append(numpy.sum(drawnFeatures*randomWeights, axis=1))
+        fractions.append(numpy.sum(drawnFractions*randomWeights, axis=1))
+
+    return numpy.array(mixtures).T, numpy.array(fractions).T
