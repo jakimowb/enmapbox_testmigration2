f2ac0186d7f957a4286eec3d8086c347a6ea20ef
Franz Schug <franz.schug@geo.hu-berlin.de>
2017-07-28 12:01:25 +0200

    Individual metadata available

--- enmapbox/apps/metadataeditorapp/metadataeditor.py
+++ enmapbox/apps/metadataeditorapp/metadataeditor.py
@@ -35,37 +35,108 @@ class NoDataValue(pTypes.GroupParameter):
     def getParameter(self):
         return self.a
 
+class DefaultBands(pTypes.GroupParameter):
+    def __init__(self, dataset, rastercount, **opts):
+        opts['type'] = 'int'
+        opts['value'] = 0
+        pTypes.GroupParameter.__init__(self, **opts)
+
+        self.addChildren([
+            {'name': 'Red', 'type': 'int', 'value': dataset[0], 'limits': (0, rastercount)},
+            {'name': 'Green', 'type': 'int', 'value': dataset[1], 'limits': (0, rastercount)},
+            {'name': 'Blue', 'type': 'int', 'value': dataset[2], 'limits': (0, rastercount)},
+            {'name': 'Alpha', 'type': 'int', 'value': dataset[3], 'limits': (0, rastercount)},
+        ])
+
+        self.redParam = self.param('Red')
+        self.greenParam = self.param('Green')
+        self.blueParam = self.param('Blue')
+        self.alphaParam = self.param('Alpha')
+
+    def getRedParameter(self):
+        return self.redParam
+
+    def getGreenParameter(self):
+        return self.greenParam
+
+    def getBlueParameter(self):
+        return self.blueParam
+
+    def getAlphaParameter(self):
+        return self.alphaParam
+
 class Bands(pTypes.GroupParameter):
-    def __init__(self, dataset, **opts):
+    def __init__(self, dataset, ind, **opts):
         opts['type'] = 'bool'
         opts['value'] = True
 
         pTypes.GroupParameter.__init__(self, **opts)
 
-        self.addChild({'name': 'Band', 'type': 'str', 'value': str(dataset.GetDescription()), 'readonly': True})
-        self.addChild({'name': 'Wavelength', 'type': 'str', 'value': str(dataset.GetMetadataItem('wavelength')), 'readonly': True}) #, pszDomain='ENVI'))
-        self.addChild({'name': 'Wavelength Unit', 'type': 'str', 'value': str(dataset.GetMetadataItem('wavelength unit')), 'readonly': True}) #, domain='ENVI'))
-        self.addChild({'name': 'Band NoData Value', 'type': 'str', 'value': str(dataset.GetNoDataValue()),
-                 'readonly': False})
-        self.addChild({'name': 'Description', 'type': 'str', 'value': dataset.GetDescription(),
-                 'readonly': True})
+        self.addChildren([
+            {'name': 'Band Number', 'type': 'str', 'value': str(ind), 'readonly': True},
+            {'name': 'Band Name', 'type': 'str', 'value': dataset.GetDescription(),
+             'readonly': True},
+            {'name': 'Wavelength', 'type': 'str', 'value': str(dataset.GetMetadataItem('wavelength')), 'readonly': True},
+            {'name': 'Wavelength Unit', 'type': 'str', 'value': str(dataset.GetMetadataItem('wavelength_units')),
+             'readonly': True},
+            {'name': 'Band NoData Value', 'type': 'str', 'value': str(dataset.GetNoDataValue()),
+             'readonly': False},
+            ])
 
         self.a = self.param('Band NoData Value')
 
     def getParameter(self):
         return self.a
 
+
+## this group includes a menu allowing the user to add new parameters into its child list
+class FreeMetadataDomains(pTypes.GroupParameter):
+    def __init__(self, dataset, **opts):
+        opts['type'] = 'group'
+        opts['addText'] = "Add Domain"
+        opts['renamable'] = True
+        opts['removable'] = True
+        pTypes.GroupParameter.__init__(self, **opts)
+
+        if dataset != None:
+            a = dataset.GetMetadataDomainList()
+            for index in range(0, len(a) - 1):
+                theseGrandchildren = dataset.GetMetadata(domain=a[index])
+                orderedGrandchildren = collections.OrderedDict(sorted(theseGrandchildren.items()))
+
+                domain = FreeMetadata(name=a[index], dataset=orderedGrandchildren)
+
+                self.addChild(domain)
+
+    def addNew(self):
+        self.addChild(FreeMetadata(name="New Domain %d" % (len(self.childs) + 1), dataset = None))
+        #self.addChild(
+        #    dict(name="New Domain %d" % (len(self.childs) + 1), type='group', value='', removable=True,
+        #        renamable=True))
+
+## this group includes a menu allowing the user to add new parameters into its child list
+class FreeMetadata(pTypes.GroupParameter):
+    def __init__(self, dataset, **opts):
+        opts['type'] = 'group'
+        opts['addText'] = "Add Parameter"
+        pTypes.GroupParameter.__init__(self, **opts)
+
+        if dataset != None:
+            for jndex in range(0, len(dataset)):
+                self.addChild({'name': str(dataset.keys()[jndex]), 'type': 'str',
+                         'value': dataset.values()[jndex], 'readonly': False, 'renamable': True,
+                         'removable': True})
+
+    def addNew(self):
+        self.addChild(dict(name="New Parameter %d" % (len(self.childs) + 1), type='str', value='', removable=True,
+                            renamable=True))
+
+
 class Win(QtGui.QWidget):
 
     inDS = None
 
-    params = [
-        {'name': 'General', 'type': 'group', 'children': []},
-        {'name': 'Image NoData Value', 'type': 'group', 'children': []},
-        {'name': 'Coordinate Reference System', 'type': 'group', 'children': []},
-        {'name': 'Bands', 'type': 'group', 'children': []},
-        {'name': 'Other', 'type': 'group', 'children': []}
-    ]
+    params = []
 
     bandNoDataValues = []
     nodataValue = None
@@ -76,6 +147,11 @@ class Win(QtGui.QWidget):
     generalChildren = []
     crsChildren = []
     otherChildren = []
+
+    redBand = None
+    greenBand = None
+    blueBand = None
+    alphaBand = None
 
     t = ParameterTree()
 
@@ -117,7 +193,7 @@ class Win(QtGui.QWidget):
         layout.addWidget(savebtn, 3, 0, 1, 4)
         layout.addWidget(closebtn, 3, 4, 1, 4)
 
-        self.resize(800,800)
+        self.resize(600,600)
 
         self.t.setParameters(self.p, showTop=False)
         #self.p.sigTreeStateChanged.connect(self.change)
@@ -143,6 +219,7 @@ class Win(QtGui.QWidget):
             params = self.setMetadataTree()
             self.populateMetadata(params)
 
+
     def ndvChanged(self, i, data):
         try:
             float(data)
@@ -157,6 +234,22 @@ class Win(QtGui.QWidget):
         if data != self.ndv.getParameter().value():
             self.ndv.getParameter().setValue(str('is defined per band. type here to set nodata value for all bands.'))
 
+    def defaultRedChanged(self, i, data):
+        self.redBand = data
+        print "red changed"
+
+    def defaultGreenChanged(self, i, data):
+        self.greenBand = data
+        print "green changed"
+
+    def defaultBlueChanged(self, i, data):
+        self.blueBand = data
+        print "blue changed"
+
+    def defaultAlphaChanged(self, i, data):
+        self.alphaBand = data
+        print "alpha changed"
+
     def setMetadataTree(self):
         from osgeo import osr
 
@@ -167,28 +260,16 @@ class Win(QtGui.QWidget):
             {'name': 'Image NoData Value', 'type': 'group', 'children': []},
             {'name': 'Coordinate Reference System', 'type': 'group', 'children': []},
             {'name': 'Bands', 'type': 'group', 'children': []},
-            {'name': 'Other', 'type': 'group', 'children': []}
+            {'name': 'Free Metadata by Domain (right click to rename and remove)', 'type': 'group', 'children': []}
         ]
+
+        self.redBand = 0
+        self.greenBand = 0
+        self.blueBand = 0
+        self.alphaBand = 0
 
         self.ndv = NoDataValue(name='NoData Value')
         self.ndv.getParameter().sigValueChanged.connect(lambda param, data: self.ndvChanged(0, data))
-
-        self.generalChildren = [
-            {'name': 'File Path', 'type': 'str', 'value': self.inputFile.currentText(), 'readonly': True},
-            {'name': 'File Type', 'type': 'str', 'value': self.inDS.GetDriver().ShortName, 'readonly': True},
-            {'name': 'Byte Order', 'type': 'str', 'value': '?', 'readonly': True},
-            {'name': 'Data Type', 'type': 'str', 'value': gdal.GetDataTypeName(self.inDS.GetRasterBand(1).DataType), 'readonly': True},
-            {'name': 'Interleave', 'type': 'str', 'value': '?', 'readonly': True},
-            {'name': 'Samples', 'type': 'int', 'value': self.inDS.RasterXSize, 'readonly': True},
-            {'name': 'Lines', 'type': 'int', 'value': self.inDS.RasterYSize, 'readonly': True},
-            {'name': 'Number of Bands', 'type': 'int', 'value': self.inDS.RasterCount, 'readonly': True},
-            {'name': 'Pixel Size', 'type': 'str',
-                                     'value': str(self.inDS.GetGeoTransform()[1]) + ' x ' + str(
-                                         -1 * self.inDS.GetGeoTransform()[5]),
-                                     'readonly': True},
-            {'name': 'Default Bands', 'type': 'str', 'value': '?', 'readonly': True},
-            {'name': 'Description', 'type': 'str', 'value': self.inDS.GetDescription(), 'readonly': True},
-            ]
 
         prj = self.inDS.GetProjection()
         srs = osr.SpatialReference(wkt=prj)
@@ -220,19 +301,21 @@ class Win(QtGui.QWidget):
         # crsChildren.append({'name': 'Scale Factor', 'type': 'str', 'value': parameters, 'readonly': True})
         # crsChildren.append({'name': 'Latitude of Origin', 'type': 'str', 'value': parameters, 'readonly': True})
 
-        self.otherChildren = [
-            {'name': 'Other', 'type': 'str', 'value': '?', 'readonly': True},
-        ]
+        domains = FreeMetadataDomains(name='Free Metadata by Domain (right click to rename and remove)', dataset = self.inDS)
 
         self.nodataValue = self.inDS.GetRasterBand(1).GetNoDataValue()
         self.ndv.getParameter().setValue(str(self.nodataValue))
 
         self.bandc2 = []
 
+        keys = {'red', 'green', 'blue', 'yellow', 'orange', 'pink', 'black'}
+        d = dict.fromkeys(keys)  # dict is pre-sized to 32 empty slots
+        print "ddd"
+        print d
         for index in range(1, self.inDS.RasterCount):
             band = self.inDS.GetRasterBand(index)
 
-            self.currentBand = Bands(name=('Band ' + str(index)), dataset=band)
+            self.currentBand = Bands(name=('Band ' + str(index)), dataset=band, ind = index)
             self.currentBand.getParameter().sigValueChanged.connect(lambda param, data: self.bandNdvChanged(index, data))
             self.bandc2.append(self.currentBand)
 
@@ -241,11 +324,55 @@ class Win(QtGui.QWidget):
                 self.ndv.getParameter().setValue(str('is defined per band. type here to set nodata value for all bands.'))
                 break
 
+            #print band.GetRasterColorInterpretation(0)
+            print "color"
+            print band.GetRasterColorInterpretation()
+            if band.GetRasterColorInterpretation() == 3:
+                print "check red"
+                print self.redBand
+                self.redBand = index
+            elif band.GetRasterColorInterpretation() == 4:
+                print "check green"
+                print self.greenBand
+                self.greenBand = index
+            elif band.GetRasterColorInterpretation() == 5:
+                print "check blue"
+                print self.blueBand
+                self.blueBand = index
+            elif band.GetRasterColorInterpretation() == 6:
+                print "check alpha"
+                print self.alphaBand
+                self.alphaBand = index
+
+        print self.inDS
+        print self.inDS.GetDescription()
+
+        defaultBands = DefaultBands(name='Default Bands', dataset=[self.redBand, self.greenBand, self.blueBand, self.alphaBand], rastercount=self.inDS.RasterCount)
+        self.generalChildren = [
+            {'name': 'File Path', 'type': 'str', 'value': self.inputFile.currentText(), 'readonly': True},
+            {'name': 'File Type', 'type': 'str', 'value': self.inDS.GetDriver().ShortName, 'readonly': True},
+            {'name': 'Data Type', 'type': 'str', 'value': gdal.GetDataTypeName(self.inDS.GetRasterBand(1).DataType), 'readonly': True},
+            {'name': 'Samples', 'type': 'int', 'value': self.inDS.RasterXSize, 'readonly': True},
+            {'name': 'Lines', 'type': 'int', 'value': self.inDS.RasterYSize, 'readonly': True},
+            {'name': 'Number of Bands', 'type': 'int', 'value': self.inDS.RasterCount, 'readonly': True},
+            {'name': 'Pixel Size', 'type': 'str',
+                                     'value': str(self.inDS.GetGeoTransform()[1]) + ' x ' + str(
+                                         -1 * self.inDS.GetGeoTransform()[5]),
+                                     'readonly': True},
+            defaultBands,
+            {'name': 'Description', 'type': 'str', 'value': self.inDS.GetDescription(), 'readonly': False},
+            ]
+
+        defaultBands.getRedParameter().sigValueChanged.connect(lambda param, data: self.defaultRedChanged(index, data))
+        defaultBands.getGreenParameter().sigValueChanged.connect(lambda param, data: self.defaultGreenChanged(index, data))
+        defaultBands.getBlueParameter().sigValueChanged.connect(lambda param, data: self.defaultBlueChanged(index, data))
+        defaultBands.getAlphaParameter().sigValueChanged.connect(lambda param, data: self.defaultAlphaChanged(index, data))
+
         self.params[0].update({'children': self.generalChildren})
         self.params[1] = self.ndv
         self.params[2].update({'children': self.crsChildren})
         self.params[3].update({'children': self.bandc2})
-        self.params[4].update({'children': self.otherChildren})
+        self.params[4] = domains
 
         end = time.time()
         print("setup time")
@@ -271,7 +398,7 @@ class Win(QtGui.QWidget):
 
     def populateMetadata(self, params):
         self.p = Parameter.create(name='params', type='group', children=params)
-        self.p.sigTreeStateChanged.connect(self.change)
+        #self.p.sigTreeStateChanged.connect(self.change)
         self.t.setParameters(self.p, showTop=False)
 
     def validatePath(self, *args, **kwds):
@@ -297,35 +424,46 @@ class Win(QtGui.QWidget):
             return result
 
     ## If anything changes in the tree, print a message
-    def change(self, param, changes):
-        # print("tree changes:")
-        for param, change, data in changes:
-            path = self.p.childPath(param)
-            if path is not None:
-                childName = '.'.join(path)
-            else:
-                childName = param.name()
-
-            #print('  parameter: %s'% childName)
-            #print('  change:    %s'% change)
-            #print('  data:      %s'% str(data))
-            #print('  ----------')
+    #def change(self, param, changes):
+    #    # print("tree changes:")
+    #    for param, change, data in changes:
+    #        path = self.p.childPath(param)
+    #        if path is not None:
+    #            childName = '.'.join(path)
+    #        else:
+    #            childName = param.name()
 
     def saveMetadata(self):
 
-        #({'name': 'Default Bands', 'type': 'str', 'value': '?', 'readonly': True})
-        #({'name': 'Description', 'type': 'str', 'value': '?', 'readonly': True})
+        #todo: Save free metadata
+
+
+        # Set image description
+        print self.inDS.GetDescription()
+        self.inDS.SetDescription(str("bla"))
+        print self.inDS.GetDescription()
 
         # Set NoData Values
         for index in range(1, self.inDS.RasterCount):
-
-            #print(index)
-            #print(self.bandChildren[index - 1]['children'][3]['value'])
-
             try:
                 self.inDS.GetRasterBand(index).SetNoDataValue(float(self.bandc2[index - 1].getParameter().value()))
             except:
                 self.inDS.GetRasterBand(index).DeleteNoDataValue()
+
+        # Set default bands
+
+        if self.redBand != 0:
+            print "reeedd"
+            print self.redBand
+            print self.inDS.GetRasterBand(self.redBand)
+            self.inDS.GetRasterBand(self.redBand).SetRasterColorInterpretation(int(3))
+            print self.inDS.GetRasterBand(self.redBand).GetRasterColorInterpretation()
+        if self.greenBand != 0:
+            self.inDS.GetRasterBand(self.redBand).SetRasterColorInterpretation(4)
+        if self.blueBand != 0:
+            self.inDS.GetRasterBand(self.redBand).SetRasterColorInterpretation(5)
+        if self.alphaBand != 0:
+            self.inDS.GetRasterBand(self.redBand).SetRasterColorInterpretation(6)
 
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
