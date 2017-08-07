1cfd648b3f470a4cdc23785f950297e130ba1aa0
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2017-08-07 16:24:41 +0200

    updated pyqtgraph

--- make/updateexternals.sh
+++ make/updateexternals.sh
@@ -4,7 +4,7 @@
 #has not been added as remote repository
 
 #update pyqtgraph
-#git remote add pyqtgraph https://github.com/pyqtgraph/pyqtgraph.git
+git remote add pyqtgraph https://github.com/pyqtgraph/pyqtgraph.git
 git fetch pyqtgraph
 git read-tree --prefix=site-packages/pyqtgraph -u pyqtgraph/master
 git commit -m "updated pyqtgraph"
@@ -13,14 +13,12 @@ git commit -m "updated pyqtgraph"
 git remote add hub-datacube https://bitbucket.org/hu-geomatics/hub-datacube.git
 git fetch hub-datacube
 git read-tree --prefix=site-packages/hubdc -u hub-datacube/master:hubdc
-#git read-tree --prefix=doc/hubdc -u hub-datacube/master:doc
 git commit -m "updated hub-datacube"
 
 #update hub-workflow
-#git remote add hub-workflow https://bitbucket.org/hu-geomatics/hub-workflow.git
+git remote add hub-workflow https://bitbucket.org/hu-geomatics/hub-workflow.git
 git fetch hub-workflow
 git read-tree --prefix=site-packages/hubflow -u hub-workflow/master:hubflow
-#git read-tree --prefix=doc/hubflow -u hub-workflow/master:doc
 git commit -m "updated hub-workflow"
 
 
