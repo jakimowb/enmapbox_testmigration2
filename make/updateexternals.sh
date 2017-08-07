3b2afe3a1db50d156942be37368b4717fb593744
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2017-08-07 17:08:07 +0200

    updated make/updateexternals.sh

--- make/updateexternals.sh
+++ make/updateexternals.sh
@@ -4,22 +4,33 @@
 #has not been added as remote repository
 
 #update pyqtgraph
-git remote add pyqtgraph https://github.com/pyqtgraph/pyqtgraph.git
+a=$(git ls-remote --exit-code pyqtgraph | grep pyqtgraph)
+if $("$a" = ""); then
+    git remote add pyqtgraph https://github.com/pyqtgraph/pyqtgraph.git
+fi
+
 git fetch pyqtgraph
 git read-tree --prefix=site-packages/pyqtgraph -u pyqtgraph/master
-git commit -m "updated pyqtgraph"
+#git commit -m "updated pyqtgraph"
 
 #update hub-datacube
-git remote add hub-datacube https://bitbucket.org/hu-geomatics/hub-datacube.git
+a=$(git ls-remote --exit-code hub-datacube | grep hub-datacube)
+if $("$a" = ""); then
+    git remote add hub-datacube https://bitbucket.org/hu-geomatics/hub-datacube.git
+fi
+
 git fetch hub-datacube
 git read-tree --prefix=site-packages/hubdc -u hub-datacube/master:hubdc
-git commit -m "updated hub-datacube"
+#git commit -m "updated hub-datacube"
 
 #update hub-workflow
-git remote add hub-workflow https://bitbucket.org/hu-geomatics/hub-workflow.git
+a=$(git ls-remote --exit-code hub-workflow | grep hub-workflow)
+if $("$a" = ""); then
+    git remote add hub-datacube https://bitbucket.org/hu-geomatics/hub-workflow.git
+fi
 git fetch hub-workflow
 git read-tree --prefix=site-packages/hubflow -u hub-workflow/master:hubflow
-git commit -m "updated hub-workflow"
+#git commit -m "updated hub-workflow"
 
 
 
