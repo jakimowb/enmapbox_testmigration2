2993f91d8831aa2b88c54ab43e8db4608fdff955
Martin Danner <martin.danner@iggf.geo.uni-muenchen.de>
2017-08-17 15:26:08 +0200

    Commit für Matthias W.

--- enmapbox/apps/the_lmu_app/sandbox.py
+++ enmapbox/apps/the_lmu_app/sandbox.py
@@ -47,15 +47,14 @@ def sandboxGuiOnly():
     """
     from enmapbox.gui.sandbox import initQgisEnvironment
     qgsApp = initQgisEnvironment()
-
     from GUI_ISD import UiFunc
-    ui = UiFunc()
-    ui.gui.show()
+    gui1 = UiFunc()
+    gui1.gui.show()
     qgsApp.exec_()
     qgsApp.quit()
 
 if __name__ == '__main__':
-    if False:
+    if True:
         sandboxGuiOnly()
     else:
         sandboxWithEnMapBox(loadPF=True)
