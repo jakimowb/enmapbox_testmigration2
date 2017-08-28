b2f75cf9b7941516f2bb44d61d17cc6f50022b4c
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2017-08-28 12:00:34 +0200

    added objbrowser to list of remote repos

--- make/updateexternals.py
+++ make/updateexternals.py
@@ -84,6 +84,11 @@ RemoteInfo.create(r'https://bitbucket.org/hu-geomatics/hub-workflow.git',
                   prefixLocal = r'site-packages/hubflow',
                   prefixRemote = r'hubflow')
 
+RemoteInfo.create(r'https://github.com/titusjan/objbrowser.git',
+                  prefixLocal = r'site-packages/objbrowser',
+                  prefixRemote = r'objbrowser')
+
+
 def updateRemote(remoteInfo):
     if isinstance(remoteInfo, str):
         remoteInfo = REMOTEINFOS[remoteInfo]
@@ -142,7 +147,7 @@ if __name__ == "__main__":
 
     #update remotes
     to_update = ['hub-datacube', 'hub-workflow','enmap-box-geoalgorithmsprovider']#enmap-box-geoalgorithmsprovider
-    #to_update = []
+    #to_update = ['objbrowser']
     for p in to_update:
         updateRemote(p)
 
