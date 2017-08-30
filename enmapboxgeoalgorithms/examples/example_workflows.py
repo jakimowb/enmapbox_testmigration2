ff50a4447f24b4cd0b3f07266223bc15b0a937d9
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2017-08-30 18:17:43 +0200

    updates

--- enmapboxgeoalgorithms/examples/example_workflows.py
+++ enmapboxgeoalgorithms/examples/example_workflows.py
@@ -1,10 +1,12 @@
+import objbrowser
+
 from os.path import join
 from tempfile import gettempdir
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.multioutput import MultiOutputRegressor
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.svm import SVR, LinearSVR
 
 from hubflow.types import *
@@ -45,7 +47,7 @@ def synthMixRegressionWorkflow():
     useSVRInstaedOfRFR = False
     if useSVRInstaedOfRFR:
         # todo: SVR is not working correctly
-        assert 0
+        #assert 0
         svr = make_pipeline(StandardScaler(), SVR(epsilon=0.0001, tol=0.0001))
         tunedSVR = GridSearchCV(cv=3, estimator=svr, param_grid = {'svr__kernel': ['rbf'],
                                                                    'svr__gamma': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
@@ -61,11 +63,12 @@ def synthMixRegressionWorkflow():
     fractions.asProbability(classDefinition=classDefinition).asClassColorRGBImage(imageFilename=join(outdir, 'fractionsRGB.img'), overwrite=overwrite)
 
     # create testing data
-    vectorClassification = VectorClassification(filename=enmapboxtestdata.landcover, classDefinition=classDefinition, idAttribute='Level_2_ID')
+    vectorClassification = VectorClassification(filename=enmapboxtestdata.landcover, classDefinition=classDefinition, idAttribute='Level_2_ID',
+                                                minOverallCoverage=1., minWinnerCoverage=0.5)
 
     # - rasterize ground truth classification into probabilities
     gtFractions = vectorClassification.rasterizeAsProbability(probabilityFilename=join(outdir, 'gtFractions.img'), grid=image.pixelGrid,
-                                                              minOverallCoverage=0.8, oversampling=10, overwrite=overwrite)
+                                                              oversampling=10, overwrite=overwrite)
     gtFractions.asClassColorRGBImage(imageFilename=join(outdir, 'gtFractionsRGB.img'), overwrite=overwrite)
 
     # accuracy assessment
@@ -105,7 +108,74 @@ def classificationWorkflow():
     # accuracy assessment on training data
     classification.assessClassificationPerformance(classification=gtClassification).report().saveHTML(filename=join(outdir, 'report.html'), open=True)
 
+def debug_synthMixRegressionWorkflow():
+
+    outdir = join(gettempdir(), 'hubflow_synthMixRegressionWorkflow')
+    overwrite = True
+
+    print('\n### SyntMixRegression Workflow ###'
+          '\nOutput directory: {}'.format(outdir))
+
+    # create training data
+    # - import ENVI speclib
+    unsupervisedSample = UnsupervisedSample.fromENVISpectralLibrary(filename=enmapboxtestdata.speclib)
+
+    # - label spectra
+    classDefinition = ClassDefinition(names=unsupervisedSample.metadata['level 2 class names'][1:],
+                                      lookup=unsupervisedSample.metadata['level 2 class lookup'][3:])
+
+    classificationSample = unsupervisedSample.classifyByClassName(names=unsupervisedSample.metadata['level 2 class spectra names'],
+                                                                  classDefinition=classDefinition)
+
+    # - scale spectra into image data range
+    classificationSample.scaleFeaturesInplace(factor=10000.)
+
+    # - generate synthetic mixtures
+    probabilitySamplePure = classificationSample.asProbabilitySample()
+    probabilitySampleMixed = classificationSample.synthMix(mixingComplexities={2:0.7, 3:0.3}, classLikelihoods='proportional', n=1000)
+    probabilitySample = ProbabilitySample(features=numpy.hstack((probabilitySamplePure.features, probabilitySampleMixed.features)),
+                                          labels=numpy.hstack((probabilitySamplePure.labels, probabilitySampleMixed.labels)),
+                                          classDefinition=probabilitySamplePure.classDefinition)
+    #probabilitySample = probabilitySample
+
+    # - subset output classes to be modeled
+    probabilitySample = probabilitySample.subsetClassesByName(names=['Tree'])
+
+    # fit model and predict fraction image
+    image = Image(filename=enmapboxtestdata.enmap)
+
+    svr = SVR(epsilon=0.00001, tol=0.00001, verbose=True)
+    tunedSVR = GridSearchCV(cv=3, estimator=svr, scoring='mean_absolute_error', verbose=True,
+                            param_grid = {'kernel': ['rbf'],
+                                          'gamma': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+                                          'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]})
+    scaledAndTunedSVR = make_pipeline(MinMaxScaler(), tunedSVR)
+    scaledAndTunedSVR = make_pipeline(MinMaxScaler(), SVR(kernel='rbf', C=10000, gamma=0.0000001))
+
+    #scaledAndTunedSVR = LinearSVR(C=100000, epsilon=0., tol=0.00001)
+    #scaledAndTunedSVR = make_pipeline(MinMaxScaler(), RandomForestRegressor())
+    #scaledAndTunedSVR = RandomForestRegressor()
+
+    #regressor = Regressor(sklEstimator=MultiOutputRegressor(estimator=scaledAndTunedSVR))
+    regressor = Regressor(sklEstimator=scaledAndTunedSVR)
+
+    regressor.fit(sample=probabilitySample)
+    fractions = regressor.predict(predictionFilename=join(outdir, 'fractions.img'), image=image, overwrite=overwrite)
+
+    # create testing data
+    vectorClassification = VectorClassification(filename=enmapboxtestdata.landcover, classDefinition=classDefinition, idAttribute='Level_2_ID',
+                                                minOverallCoverage=1.)
+
+    # - rasterize ground truth classification into probabilities
+    gtFractions = vectorClassification.rasterizeAsProbability(probabilityFilename=join(outdir, 'gtFractions.img'), grid=image.pixelGrid,
+                                                              minOverallCoverage=0.8, oversampling=10, overwrite=overwrite)
+    gtFractions = gtFractions.subsetClassesByName(filename=join(outdir, 'fractions1Class.img'), names=['Tree'])
+
+    # accuracy assessment
+    fractions.assessRegressionPerformance(regression=gtFractions).report().saveHTML(filename=join(outdir, 'report2.html'), open=True)
 
 if __name__ == '__main__':
-    synthMixRegressionWorkflow()
-    classificationWorkflow()
+    #synthMixRegressionWorkflow()
+    #classificationWorkflow()
+    #regressionWorkflow_oldTestdata()
+    debug_synthMixRegressionWorkflow()
