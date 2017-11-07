b2278374e47c70ef4a278a299549409435520271
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2017-11-07 15:51:02 +0100

    pulled updates

--- make/updateexternals.py
+++ make/updateexternals.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
     #update remotes
     to_update = ['hub-datacube', 'hub-workflow','enmap-box-testdata','enmap-box-geoalgorithmsprovider']#enmap-box-geoalgorithmsprovider
     #to_update = ['objbrowser']
-    to_update = ['virtual-raster-builder']
+    #to_update = ['virtual-raster-builder']
     #to_update = ['hub-datacube']
     #to_update = ['enmap-box-testdata'] + to_update
     for p in to_update:
