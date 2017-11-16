786938c007830e4ac37a00592e6c9ef09a913e8b
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2017-11-16 15:48:18 +0100

    enmapbox.__init__.py added HOMEPAGE, REPOSITORY and TRACKER enmapbox_gui.ui disabled forced dock tabbing

--- enmapbox/__init__.py
+++ enmapbox/__init__.py
@@ -1,2 +1,5 @@
 __version__ = '3.0.0 beta'
 
+HOMEPAGE = 'https://bitbucket.org/hu-geomatics/enmap-box'
+REPOSITORY = 'https://bitbucket.org/hu-geomatics/enmap-box.git'
+TRACKER = 'https://bitbucket.org/hu-geomatics/enmap-box/issues'
