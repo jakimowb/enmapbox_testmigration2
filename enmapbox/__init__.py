f69885f955228f58747e8ca214f1673bf1ee7dc9
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2018-01-10 10:18:13 +0100

    updated metadata for version: "20180110T1018"

--- enmapbox/__init__.py
+++ enmapbox/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '20180110T1016'
+__version__ = '20180110T1018'
 
 HOMEPAGE = 'https://bitbucket.org/hu-geomatics/enmap-box'
 REPOSITORY = 'https://bitbucket.org/hu-geomatics/enmap-box.git'
