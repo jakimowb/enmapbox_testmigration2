b9b4b9a5153b647d6865135e6abb65bb02ce12f5
Benjamin Jakimow <benjamin.jakimow@geo.hu-berlin.de>
2018-04-25 06:05:45 +0200

    maptools.py fixed Qt imports

--- enmapbox/gui/maptools.py
+++ enmapbox/gui/maptools.py
@@ -23,14 +23,16 @@
 from qgis import *
 from qgis.core import *
 from qgis.gui import *
-
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtXml import *
-from PyQt5.QtXmlPatterns import *
-import six
-import xml.etree
-from qgis.core import *
+from qgis.PyQt.QtCore import *
+from qgis.PyQt.QtXml import *
+from qgis.PyQt.QtGui import *
+from qgis.PyQt.QtWidgets import *
+#from PyQt5.QtCore import *
+#from PyQt5.QtGui import *
+#from PyQt5.QtXml import *
+#from PyQt5.QtXmlPatterns import *
+#import six
+#import xml.etree
 
 import numpy as np
 from enmapbox.gui.utils import *
