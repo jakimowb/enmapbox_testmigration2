813292485137c95798a0fc7d225ab69dd16ce105
janzandr <andreas.rabe@geo.hu-berlin.de>
2018-04-26 11:08:40 +0200

    added example processing algorithm with many widgets

--- examples/minimumexample/exampleapp.py
+++ examples/minimumexample/exampleapp.py
@@ -24,7 +24,8 @@ import os
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMenu, QAction, QWidget, QHBoxLayout, QLabel, QPushButton
 from enmapbox.gui.applications import EnMAPBoxApplication
-from qgis.core import QgsProcessingAlgorithm, QgsProcessingFeedback, QgsProcessingContext, QgsProcessingParameterRasterLayer, QgsProcessingParameterRasterDestination, QgsProcessingParameterNumber
+from qgis.core import *
+
 
 VERSION = '0.0.1'
 LICENSE = 'GNU GPL-3'
@@ -89,7 +90,7 @@ class ExampleEnMAPBoxApp(EnMAPBoxApplication):
         :return: [list-of-GeoAlgorithms]
         """
 
-        return [ExampleGeoAlgorithm()]
+        return [ExampleGeoAlgorithm(), ExampleAlgorithmWithManyWidgets()]
 
     def startGUI(self, *args):
         """
@@ -159,6 +160,138 @@ class ExampleGeoAlgorithm(QgsProcessingAlgorithm):
         outputs = {}
         return outputs
 
+class ExampleAlgorithmWithManyWidgets(QgsProcessingAlgorithm):
+
+    P_RASTER = 'raster'
+    P_RASTER_BAND = 'raster_band'
+    P_VECTOR = 'vector'
+    P_VECTOR_FIELD = 'vector_field'
+    P_MAP = 'map'
+    P_FILE = 'file'
+    P_FOLDER = 'folder'
+    P_INTEGER = 'integer'
+    P_FLOAT = 'float'
+    P_STRING = 'string'
+    P_BOOLEAN= 'boolean'
+    P_ENUM = 'enum'
+    P_OUTPUT_RASTER = 'outraster'
+    P_OUTPUT_VECTOR = 'outvector'
+    P_OUTPUT_FILE = 'outfile'
+    P_OUTPUT_FOLDER = 'outfolder'
+
+    def group(self):
+        return APP_NAME
+
+    def name(self):
+        return 'ExampleAlgorithmWithManyWidgets'
+
+    def displayName(self):
+        return 'Example Algorithm with many Widgets'
+
+    def createInstance(self, *args, **kwargs):
+        return type(self)()
+
+    def initAlgorithm(self, configuration=None):
+
+        # some example widgets that can be used; for full list see https://qgis.org/api/annotated.html
+
+        # - different input widgets
+        self.addParameter(QgsProcessingParameterRasterLayer(name=self.P_RASTER, description='Raster Layer',
+                                                            optional=True))
+
+        self.addParameter(QgsProcessingParameterBand(name=self.P_RASTER_BAND, description='Raster Layer Band',
+                                                     defaultValue=0, parentLayerParameterName=self.P_RASTER,
+                                                     optional=True))
+
+        self.addParameter(QgsProcessingParameterVectorLayer(name=self.P_VECTOR, description='Vector Layer',
+                                                            defaultValue=None, optional=True))
+
+        self.addParameter(QgsProcessingParameterField(name=self.P_VECTOR_FIELD, description='Vector Layer Field',
+                                                      parentLayerParameterName=self.P_VECTOR,
+                                                      type=QgsProcessingParameterField.Any,
+                                                      allowMultiple=False, defaultValue=None, optional=True))
+
+        self.addParameter(QgsProcessingParameterMapLayer(name=self.P_MAP, description='Map Layer',
+                                                         defaultValue=None, optional=True))
+
+        self.addParameter(QgsProcessingParameterFile(name=self.P_FILE, description='File',
+                                                     behavior=QgsProcessingParameterFile.File, extension='',
+                                                     defaultValue=None, optional=True))
+
+        self.addParameter(QgsProcessingParameterFile(name=self.P_FOLDER, description='Folder',
+                                                     behavior=QgsProcessingParameterFile.Folder,
+                                                     defaultValue=None, optional=True))
+
+        self.addParameter(QgsProcessingParameterNumber(name=self.P_INTEGER, description='Integer',
+                                                       type=QgsProcessingParameterNumber.Integer,
+                                                       defaultValue=0, optional=False, minValue=0, maxValue=10))
+
+        self.addParameter(QgsProcessingParameterNumber(name=self.P_FLOAT, description='Float',
+                                                       type=QgsProcessingParameterNumber.Double,
+                                                       defaultValue=0.0, optional=False, minValue=0, maxValue=10))
+
+        self.addParameter(QgsProcessingParameterString(name=self.P_STRING, description='String',
+                                                       defaultValue='Hello World', multiLine=False, optional=False))
+
+        self.addParameter(QgsProcessingParameterBoolean(name=self.P_BOOLEAN, description='Boolean',
+                                                        defaultValue=True, optional=False))
+
+        self.enumOptions = ['A', 'B', 'C']
+        self.addParameter(QgsProcessingParameterEnum(name=self.P_ENUM, description='Enum',
+                                                     options=self.enumOptions, allowMultiple=False, defaultValue=0,
+                                                     optional=False))
+
+        # - different output widgets
+
+        # raster, vector, map, file, folder
+        self.addParameter(QgsProcessingParameterRasterDestination(name=self.P_OUTPUT_RASTER,
+                                                                  description='Output Raster'))
+        self.addParameter(QgsProcessingParameterVectorDestination(name=self.P_OUTPUT_VECTOR,
+                                                            description='Output Vector'))
+        self.addParameter(QgsProcessingParameterFileDestination(name=self.P_OUTPUT_FILE,
+                                                                description='Output File',
+                                                                fileFilter='txt'))
+        self.addParameter(QgsProcessingParameterFolderDestination(name=self.P_OUTPUT_FOLDER,
+                                                                  description='Output Folder', optional=True))
+
+    def processAlgorithm(self, parameters, context, feedback):
+        assert isinstance(parameters, dict)
+        assert isinstance(context, QgsProcessingContext)
+        assert isinstance(feedback, QgsProcessingFeedback)
+
+        # print all parameters to log
+        for key in sorted(parameters):
+            feedback.pushInfo('{} = {}'.format(key, repr(parameters[key])))
+
+        # create output files
+        rasterFilename = str(self.parameterAsOutputLayer(parameters, self.P_OUTPUT_RASTER, context))
+        vectorFilename = str(self.parameterAsOutputLayer(parameters, self.P_OUTPUT_VECTOR, context))
+        fileFilename = parameters[self.P_OUTPUT_FILE]
+        folderFilename = parameters[self.P_OUTPUT_FOLDER]
+        # - here comes your fancy processing algorithm!!!
+        with open(rasterFilename, 'w'): pass # is not really a raster file :-)
+        with open(vectorFilename, 'w'): pass # is not really a vector file :-)
+        with open(fileFilename, 'w'): pass
+        os.makedirs(folderFilename)
+
+        # return outputs
+        return {self.P_OUTPUT_RASTER: parameters[self.P_OUTPUT_RASTER],
+                self.P_OUTPUT_VECTOR: parameters[self.P_OUTPUT_RASTER],
+                self.P_OUTPUT_FILE: parameters[self.P_OUTPUT_RASTER],
+                self.P_OUTPUT_FOLDER: parameters[self.P_OUTPUT_RASTER]}
+
+    def shortHelpString(self, *args, **kwargs):
+        text = '<p>Here comes the HTML documentation.</p>'\
+               '<h3>With Headers...</h3>'\
+               '<p>and Hyperlinks: <a href="www.google.de">Google</a></p>'
+
+        return text
+
+    def helpString(self):
+        return self.shortHelpString()
+
+    def helpUrl(self, *args, **kwargs):
+        return 'www.google.de'
 
 
 if __name__ == '__main__':
@@ -171,7 +304,7 @@ if __name__ == '__main__':
     qgsApp = initQgisApplication()
 
 
-    if True: #test GUI without EnMAP-Box
+    if False: #test GUI without EnMAP-Box
         w = ExampleAppGUI()
         w.show()
 
