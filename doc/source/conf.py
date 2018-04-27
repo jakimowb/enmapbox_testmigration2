6552d6717003cf7ec99dd96c0113db96827a94e6
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2018-04-27 11:59:43 +0200

    added mock modules to sphinx conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,6 +18,12 @@
 #
 import os
 import sys
+import mock
+
+MOCK_MODULES = ['qgis','gdal','sklearn','numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../../'))
