bd00ce8da94eec6a5026f3f6e4e52e5162261121
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2018-08-22 13:32:13 +0200

    RTD

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='enmapbox',
+    version='0.8.15',
+    description='EnMAP-Box',
+    author='',
+    author_email='',
+    packages=[],
+    url='https://bitbucket.org/hu-geomatics/enmap-box'
+    )
