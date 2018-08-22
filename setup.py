6bf506d38fc12cbe8c1794f7ab0c316f34727af0
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2018-08-22 13:35:54 +0200

    RTD

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='enmapbox',
     version='0.8.15',
-    description='EnMAP-Box',
+    description='EnMAP-Box setup.py, bascially to run ReadTheDocs only',
     author='',
     author_email='',
     packages=[],
