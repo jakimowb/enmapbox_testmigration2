5e3638e2f3faec4abedbe513fd4f4c9554e5f576
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2018-09-03 14:52:11 +0200

    removed enpt_enmapboxapp -> distributed via pip added enpt_app/__init__.py -> Entry point for EnMAPBoxApplication loading

--- enmapbox/apps/enpt_app/__init__.py
+++ enmapbox/apps/enpt_app/__init__.py
@@ -1,18 +1,10 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for enpt_enmapboxapp."""
-
-__author__ = """Daniel Scheffler"""
-__email__ = 'danschef@gfz-potsdam.de'
-__version__ = '0.1.0'
-
-
 def enmapboxApplicationFactory(enmapBox):
     """
     Returns a list of EnMAPBoxApplications
     :param enmapBox: the EnMAP-Box instance.
     :return: EnMAPBoxApplication | [list-of-EnMAPBoxApplications]
     """
+
 
     from enpt_enmapboxapp.enpt_enmapboxapp import EnPTEnMAPBoxApp
 
