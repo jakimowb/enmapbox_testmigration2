c391cd5f325d779bc4d776490c72100b25cc7f9e
Andreas Rabe <andreas.rabe@geo.hu-berlin.de>
2018-10-23 01:28:45 +0200

    no message

--- enmapboxtestdata/__init__.py
+++ enmapboxtestdata/__init__.py
@@ -1,6 +1,7 @@
 from os.path import dirname, join
 
 enmap = join(dirname(__file__), 'enmap_berlin.bsq')
+hires = join(dirname(__file__), 'hires_berlin.bsq')
 landcover_polygons = join(dirname(__file__), 'landcover_berlin_polygon.shp')
 landcover_points = join(dirname(__file__), 'landcover_berlin_point.shp')
 library = join(dirname(__file__), 'library_berlin.sli')
