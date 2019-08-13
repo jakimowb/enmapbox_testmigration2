058b529a7af6a9352017c7c92d7c553a7dfbacc1
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2019-08-13 12:07:57 +0200

    fixed setuprepository.py
    Signed-off-by: benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>

--- make/setuprepository.py
+++ make/setuprepository.py
@@ -8,14 +8,20 @@ import os, sys
 DIR_QGIS_REPO = os.environ.get('DIR_QGIS_REPO', None)
 
 
+
 from os.path import dirname as dn
 from os.path import join as jn
 DIR_ENMAPBOX_REPO = dn(dn(__file__))
 DIR_SITEPACKAGES = jn(DIR_ENMAPBOX_REPO, 'site-packages')
 DIR_QGISRESOURCES = jn(DIR_ENMAPBOX_REPO, 'qgisresources')
+DIR_MAKE = dn(__file__)
+
+if not DIR_ENMAPBOX_REPO in sys.path:
+    sys.path.append(DIR_ENMAPBOX_REPO)
+
 
 # 1. compile all EnMAP-Box resource files (*.qrc) into corresponding python modules (*.py)
-from .compileresourcefiles import compileResourceFiles, compileQGISResourceFiles
+from make.compileresourcefiles import compileResourceFiles, compileQGISResourceFiles
 compileResourceFiles()
 
 # 2. create the qgisresource folder
