5562a72a66276b0b56c915647aaed82b8436b82c
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2019-10-02 12:29:33 +0200

    pipeline fixes
    Signed-off-by: benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,7 @@
 
-:: use this script to run unit tests locally
-::
-set CI=True
-python make/setuprepository.py
+# use this script to run unit tests locally
+#
+python runfirst.py
 
 mkdir test-reports/today
 python -m nose2 -s enmapboxtesting test_applications ; mv nose2-junit.xml test-reports/today/test_applications.xml
