463be379f8bde8d908bd779f442e07e42ab2806c
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2020-02-27 14:25:40 +0100

    CI
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- scripts/runtests.bat
+++ scripts/runtests.bat
@@ -1,19 +1,19 @@
 
-    :: use this script to run unit tests locally
-    ::
-    @echo off
-    set CI=True
-    
-    WHERE python3 >nul 2>&1 && (
-        echo Found "python3" command
-        set PYTHON=python3
-    ) || (
-        echo Did not found "python3" command. use "python" instead
-        set PYTHON=python
-    )
-    
-    start %PYTHON% make/setuprepository.py
-    
+:: use this script to run unit tests locally
+::
+@echo off
+set CI=True
+
+WHERE python3 >nul 2>&1 && (
+    echo Found "python3" command
+    set PYTHON=python3
+) || (
+    echo Did not found "python3" command. use "python" instead
+    set PYTHON=python
+)
+
+start %PYTHON% scripts/setuprepository.py
+
 %PYTHON% -m coverage run --rcfile=.coveragec   enmapboxtesting/test_applications.py
 %PYTHON% -m coverage run --rcfile=.coveragec --append  enmapboxtesting/test_crosshair.py
 %PYTHON% -m coverage run --rcfile=.coveragec --append  enmapboxtesting/test_cursorlocationsvalues.py
