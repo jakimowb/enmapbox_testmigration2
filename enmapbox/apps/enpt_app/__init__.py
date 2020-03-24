856c9da20800e4436192ae45cf49605394f3d760
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2020-03-24 10:08:45 +0100

    addresses #369 enmapbox/apps/enpt_app/__init__.py - catches ModuleNotFoundError

--- enmapbox/apps/enpt_app/__init__.py
+++ enmapbox/apps/enpt_app/__init__.py
@@ -5,9 +5,8 @@ def enmapboxApplicationFactory(enmapBox):
     :return: EnMAPBoxApplication | [list-of-EnMAPBoxApplications]
     """
 
-
-    from enpt_enmapboxapp.enpt_enmapboxapp import EnPTEnMAPBoxApp
-
-    # returns a list of EnMAP-Box Applications. Usually only one is returned,
-    # but you might provide as many as you like.
-    return [EnPTEnMAPBoxApp(enmapBox)]
+    try:
+        from enpt_enmapboxapp.enpt_enmapboxapp import EnPTEnMAPBoxApp
+        return [EnPTEnMAPBoxApp(enmapBox)]
+    except ModuleNotFoundError:
+        return []
