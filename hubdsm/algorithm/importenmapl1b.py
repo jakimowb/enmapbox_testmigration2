f978ede0f6847098d212d5bf3e2b1ca45e6416f5
Andreas Rabe <andreas.rabe@geo.hu-berlin.de>
2020-04-02 00:15:02 +0200

    simplified L1B import

--- hubdsm/algorithm/importenmapl1b.py
+++ hubdsm/algorithm/importenmapl1b.py
@@ -1,18 +1,20 @@
 from os.path import splitext
+from typing import Tuple
+
 from osgeo import gdal
 
 
-def importEnmapL1B(filenameMetadataXml: str, filenameVrt: str = None):
-    '''Create VRT for given L1B product.'''
-    if filenameVrt is None:
-        filenameFull = filenameMetadataXml.replace('METADATA.XML', 'EnMAP-Box_FULL.vrt')
+def importEnmapL1B(
+        filenameMetadataXml: str, filenameVnir: str = None, filenameSwir: str = None
+) -> Tuple[gdal.Dataset, gdal.Dataset]:
+    '''Return VNIR and SWIR VRT datasets with spectral information (wavelength and FWHM) and data gains/offsets.'''
+
+    if filenameVnir is None:
         filenameVnir = filenameMetadataXml.replace('METADATA.XML', 'EnMAP-Box_VNIR.vrt')
+    if filenameSwir is None:
         filenameSwir = filenameMetadataXml.replace('METADATA.XML', 'EnMAP-Box_SWIR.vrt')
-    else:
-        assert filenameVrt.lower().endswith('.vrt')
-        head, ext = splitext(filenameVrt)
-        filenameVnir = f'{head}_VNIR{ext}'
-        filenameSwir = f'{head}_SWIR{ext}'
+    assert filenameVnir.lower().endswith('.vrt')
+    assert filenameSwir.lower().endswith('.vrt')
 
     # read metadata
     import xml.etree.ElementTree as ET
@@ -23,36 +25,27 @@ def importEnmapL1B(filenameMetadataXml: str, filenameVrt: str = None):
     offsets = [item.text for item in root.findall('specific/bandCharacterisation/bandID/OffsetOfBand')]
 
     # create VRTs
-    # - VNIR
     ds = gdal.Open(filenameMetadataXml.replace('-METADATA.XML', '-SPECTRAL_IMAGE_VNIR.GEOTIFF'))
     options = gdal.TranslateOptions(format='VRT')
     dsVnir: gdal.Dataset = gdal.Translate(destName=filenameVnir, srcDS=ds, options=options)
     dsVnir.SetMetadataItem('wavelength', '{'+', '.join(wavelength[:dsVnir.RasterCount]) + '}', 'ENVI')
     dsVnir.SetMetadataItem('wavelength_units', 'nanometers', 'ENVI')
     dsVnir.SetMetadataItem('fwhm', '{'+', '.join(fwhm[:dsVnir.RasterCount]) + '}', 'ENVI')
-    # - SWIR
+
     ds = gdal.Open(filenameMetadataXml.replace('-METADATA.XML', '-SPECTRAL_IMAGE_SWIR.GEOTIFF'))
     options = gdal.TranslateOptions(format='VRT')
     dsSwir: gdal.Dataset = gdal.Translate(destName=filenameSwir, srcDS=ds, options=options)
-    assert len(wavelength[dsVnir.RasterCount:]) == dsSwir.RasterCount
-    assert len(fwhm[dsVnir.RasterCount:]) == dsSwir.RasterCount
     dsSwir.SetMetadataItem('wavelength', '{'+', '.join(wavelength[dsVnir.RasterCount:]) + '}', 'ENVI')
     dsSwir.SetMetadataItem('wavelength_units', 'nanometers', 'ENVI')
     dsSwir.SetMetadataItem('fwhm', '{'+', '.join(fwhm[dsVnir.RasterCount:]) + '}', 'ENVI')
 
-    # manipulate VRTs
-    del dsVnir
-    del dsSwir
-    index = 0
-    for filename in [filenameVnir, filenameSwir]:
-        with open(filename) as file:
-            lines = file.readlines()
-        lines2 = list()
-        for line in lines:
-            lines2.append(line)
-            if '<Description>' in line:
-                lines2.append(f'    <Scale>{gains[index]}</Scale>\n')
-                lines2.append(f'    <Offset>{offsets[index]}</Offset>\n')
-                index += 1
-        with open(filename, 'w') as file:
-            file.writelines(lines2)
+    rasterBands = list()
+    rasterBands.extend(dsVnir.GetRasterBand(i+1) for i in range(dsVnir.RasterCount))
+    rasterBands.extend(dsSwir.GetRasterBand(i+1) for i in range(dsSwir.RasterCount))
+    rasterBand: gdal.Band
+    for i, rasterBand in enumerate(rasterBands):
+        rasterBand.SetScale(float(gains[i]))
+        rasterBand.SetOffset(float(offsets[i]))
+        rasterBand.FlushCache()
+
+    return dsVnir, dsSwir
