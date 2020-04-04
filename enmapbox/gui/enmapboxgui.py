0e25f879c407408c6038ca4bcfea21d0fd593471
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2020-04-04 12:00:28 +0200

    enmapboxgui.py - re-enabled initial requirements check

--- enmapbox/gui/enmapboxgui.py
+++ enmapbox/gui/enmapboxgui.py
@@ -281,15 +281,12 @@ class EnMAPBox(QgisInterface, QObject):
 
         assert isinstance(qgsUtils.iface, QgisInterface)
 
-        #self.mCurrentSpectra = []  # set of currently selected spectral profiles
         self.mCurrentMapLocation = None
 
         # define managers
-
         from enmapbox.gui.datasourcemanager import DataSourceManager
         from enmapbox.gui.dockmanager import DockManager
 
-        #
         splash.showMessage('Init DataSourceManager')
         self.mDataSourceManager = DataSourceManager()
         self.mDataSourceManager.sigDataSourceRemoved.connect(self.onDataSourceRemoved)
@@ -297,7 +294,8 @@ class EnMAPBox(QgisInterface, QObject):
         QgsProject.instance().layersAdded.connect(self.addMapLayers)
         QgsProject.instance().layersWillBeRemoved.connect(self.onLayersWillBeRemoved)
 
-        self._layerTreeNodes = [] #needed to keep a reference on created LayerTreeNodes
+        # needed to keep a reference on created LayerTreeNodes
+        self._layerTreeNodes = []
         self._layerTreeGroup: QgsLayerTreeGroup = None
 
         self.mDockManager = DockManager()
@@ -312,6 +310,7 @@ class EnMAPBox(QgisInterface, QObject):
         assert isinstance(root, QgsLayerTree)
         root.addedChildren.connect(self.syncHiddenLayers)
         root.removedChildren.connect(self.syncHiddenLayers)
+
         #
         self.updateCurrentLayerActions()
         self.ui.centralFrame.sigDragEnterEvent.connect(
@@ -336,7 +335,6 @@ class EnMAPBox(QgisInterface, QObject):
         self.mVectorLayerTools.sigEditingStarted.connect(self.updateCurrentLayerActions)
         self.mVectorLayerTools.sigZoomRequest.connect(self.zoomToExtent)
         self.mVectorLayerTools.sigPanRequest.connect(self.panToPoint)
-
 
         self.ui.cursorLocationValuePanel.sigLocationRequest.connect(lambda: self.setMapTool(MapTools.CursorLocation))
 
@@ -369,8 +367,7 @@ class EnMAPBox(QgisInterface, QObject):
         debugLog('Run dependency checks...')
 
         from ..dependencycheck import requiredPackages
-        #requiredPackages = []
-        if len([p for p in [] if not p.isInstalled()]) > 0:
+        if len([p for p in requiredPackages() if not p.isInstalled()]) > 0:
 
             # taken from qgsmessagebar.cpp
             # void QgsMessageBar::pushMessage( const QString &title, const QString &text, const QString &showMore, Qgis::MessageLevel level, int duration )
