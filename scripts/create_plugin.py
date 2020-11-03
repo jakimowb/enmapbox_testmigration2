2c335b0467ce3f38e8869dbeeb5348a5143d8090
Benjamin Jakimow <benjamin.jakimow@geo.hu-berlin.de>
2020-11-03 13:43:09 +0100

    refactoring

--- scripts/create_plugin.py
+++ scripts/create_plugin.py
@@ -11,7 +11,7 @@
 ***************************************************************************
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
+*   it under the terms of the GNU General Public License as pclearublished by  *
 *   the Free Software Foundation; either version 2 of the License, or     *
 *   (at your option) any later version.
                  *
@@ -28,7 +28,8 @@ import re
 import shutil
 import sys
 import typing
-
+import site
+site.addsitedir(pathlib.Path(__file__).parents[1])
 import enmapbox
 from enmapbox import DIR_REPO, __version__
 from enmapbox.externals.qps.make.deploy import QGISMetadataFileWriter
