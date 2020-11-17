2a2c98d407e339d4c66442347ecdbcc6d473b984
Benjamin Jakimow <benjamin.jakimow@geo.hu-berlin.de>
2020-11-17 14:36:30 +0100

    qps updates smaller fixes

--- enmapbox/testing.py
+++ enmapbox/testing.py
@@ -77,8 +77,11 @@ class EnMAPBoxTestCase(TestCase):
         tmpDir = cls.tempDir(cls)
         os.chdir(tmpDir)
         super().setUpClass(resources=resources)
+
         import enmapbox
         enmapbox.initAll()
+
+        s = ""
 
     def tempDir(self, subdir: str = None, cleanup: bool = False) -> pathlib.Path:
         """
