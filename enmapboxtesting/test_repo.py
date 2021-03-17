11faf08305cf1a4c72190bfe07827891f7c7f755
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2021-03-17 23:57:09 +0100

    addresses #609

--- enmapboxtesting/test_repo.py
+++ enmapboxtesting/test_repo.py
@@ -1,4 +1,3 @@
-
 import unittest
 import xmlrunner
 import typing
@@ -6,11 +5,11 @@ from urlchecker.core.check import UrlChecker, UrlCheckResult
 from urlchecker.core.urlproc import UrlCheckResult
 import pathlib
 
-
 DIR_REPO = pathlib.Path(__file__).parents[1]
 
 DIR_DOCS = DIR_REPO / 'doc'
 DIR_CODE = DIR_REPO / 'enmapbox'
+
 
 class TestRepository(unittest.TestCase):
 
@@ -25,18 +24,7 @@ class TestRepository(unittest.TestCase):
         self.assertTrue(len(checker.failed) == 0,
                         msg=f'Failed to connect to: {failed}\nService down?')
 
-    def test_urls(self):
 
-        checkerCode = UrlChecker(
-            path = DIR_DOCS,
-            file_types = ['.md', '.py', '.rst'],
-            retry_count=1,
-            timeout=5,
-            print_all=False
-        )
-        checkerCode.run()
-        results: UrlCheckResult = checkerCode.results
-        s = ""
 
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'), buffer=False)
