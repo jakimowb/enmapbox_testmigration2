a337c1829020764726f3bb244de25f3e2785c6f0
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2021-05-30 10:00:57 +0200

    backport fix plugin unloading

--- enmapbox/externals/qps/classification/classificationscheme.py
+++ enmapbox/externals/qps/classification/classificationscheme.py
@@ -918,7 +918,7 @@ class ClassificationScheme(QAbstractTableModel):
             return None
 
     def addClass(self, c, index=None):
-        warnings.warn('Use insert class', DeprecationWarning)
+        warnings.warn('Use insert class', DeprecationWarning, stacklevel=2)
 
     def insertClass(self, c, index=None):
         """
