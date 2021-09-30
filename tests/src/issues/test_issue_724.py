1a62ebcac5dac5c3b6ce318c8d7a619bff919f21
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2021-09-30 08:25:57 +0200

    addresses #724 SpectraLibrary default provider set to 'ogr'

---
+++ tests/src/issues/test_issue_724.py
@@ -0,0 +1,61 @@
+
+import unittest
+import xmlrunner
+
+from enmapbox.externals.qps.speclib.core.spectrallibrary import SpectralLibraryUtils
+from enmapbox.gui.datasourcemanager import DataSourceTreeView, DataSourceManagerTreeModel, DataSourceManager, \
+    HubFlowPyObjectTreeNode
+from qgis.core import QgsProject, QgsMapLayer, QgsRasterLayer, QgsVectorLayer, \
+    QgsLayerTree, QgsProcessingAlgorithm, QgsProcessingParameterRasterLayer, QgsProcessingParameterDefinition
+from qgis.gui import QgisInterface
+from qgis.PyQt.QtGui import QIcon
+from qgis.core import QgsApplication
+from qgis.PyQt.QtCore import QResource
+from enmapbox.testing import TestObjects, EnMAPBoxTestCase
+from enmapbox.gui.enmapboxgui import EnMAPBox, EnMAPBoxSplashScreen
+from enmapbox.gui.docks import *
+from enmapbox.gui.mapcanvas import *
+from enmapbox.gui import *
+
+
+class TestIssue(EnMAPBoxTestCase):
+
+    def tearDown(self):
+
+        emb = EnMAPBox.instance()
+        if isinstance(emb, EnMAPBox):
+            emb.close()
+
+        assert EnMAPBox.instance() is None
+
+        QgsProject.instance().removeAllMapLayers()
+
+        super().tearDown()
+
+    def test_issue_724(self):
+
+        # create vectorlayer
+        path = pathlib.Path('~').expanduser() / 'Downloads' / 'library.gpkg'
+        self.assertTrue(path.is_file())
+
+        from enmapbox.externals.qps.speclib.core import EDITOR_WIDGET_REGISTRY_KEY
+        filename = path.as_posix()
+
+        vl = QgsVectorLayer(filename)
+        sl1 = SpectralLibraryUtils.readFromSource(filename)
+        sl2 = SpectralLibrary(filename)
+        for lyr in [vl, sl1, sl2]:
+            self.assertIsInstance(lyr, QgsVectorLayer)
+            self.assertTrue(lyr.featureCount() > 0)
+            print(f'{lyr.id()}:{lyr.source()}')
+            print(f'typename: {lyr.fields().field("profiles").typeName()}')
+            print(f'editor: {lyr.fields().field("profiles").editorWidgetSetup().type()}')
+
+        # self.assertTrue(vl.fields().field('profiles').editorWidgetSetup().type() == '')
+        self.assertTrue(sl1.fields().field('profiles').editorWidgetSetup().type() == EDITOR_WIDGET_REGISTRY_KEY)
+        self.assertTrue(sl2.fields().field('profiles').editorWidgetSetup().type() == EDITOR_WIDGET_REGISTRY_KEY)
+
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'), buffer=False)
