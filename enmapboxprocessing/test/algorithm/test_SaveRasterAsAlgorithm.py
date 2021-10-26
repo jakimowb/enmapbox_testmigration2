b12fbfb0e8c8fe755af350dbd15b2f1eada5353d
janzandr <andreas.rabe@geo.hu-berlin.de>
2021-10-26 13:33:20 +0200

    resolves #814

--- enmapboxprocessing/test/algorithm/test_SaveRasterAsAlgorithm.py
+++ enmapboxprocessing/test/algorithm/test_SaveRasterAsAlgorithm.py
@@ -1,7 +1,7 @@
 import numpy as np
 from qgis._core import QgsRasterLayer
 
-from enmapbox.exampledata import enmap
+from enmapbox.exampledata import enmap, hires
 from enmapboxprocessing.algorithm.saverasterlayerasalgorithm import SaveRasterAsAlgorithm
 from enmapboxprocessing.rasterreader import RasterReader
 from enmapboxprocessing.test.algorithm.testcase import TestCase
@@ -17,6 +17,18 @@ class TestTranslateAlgorithm(TestCase):
         }
         result = self.runalg(alg, parameters)
         gold = RasterReader(enmap).array()
+        lead = RasterReader(result[alg.P_OUTPUT_RASTER]).array()
+        self.assertEqual(gold[0].dtype, lead[0].dtype)
+        self.assertEqual(np.sum(gold), np.sum(lead))
+
+    def test_issue814(self):
+        alg = SaveRasterAsAlgorithm()
+        parameters = {
+            alg.P_RASTER: QgsRasterLayer(hires),
+            alg.P_OUTPUT_RASTER: self.filename('raster.tif')
+        }
+        result = self.runalg(alg, parameters)
+        gold = RasterReader(hires).array()
         lead = RasterReader(result[alg.P_OUTPUT_RASTER]).array()
         self.assertEqual(gold[0].dtype, lead[0].dtype)
         self.assertEqual(np.sum(gold), np.sum(lead))
