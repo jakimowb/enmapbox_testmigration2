afa46f91285eeb70eb91435cabb414c023c05825
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2022-01-26 22:08:23 +0100

    cleanup snippets

--- scripts/issue_tracker_stats.py
+++ scripts/issue_tracker_stats.py
@@ -1,27 +1,57 @@
-
-
+import os
+import re
 import typing
 import pathlib
 import json
 import datetime
 import csv
+from xlsxwriter.workbook import Workbook
+# import pandas as pd
+"""
+Syntax github issue request:
+https://docs.github.com/en/search-github/searching-on-github/searching-issues-and-pull-requests
+
+author:jakimowb type:issue created:>=2022-07-01 created:<=2022-12-31 
+
+"""
 # 1. open bitbucket,
 # goto repository settings -> issues -> Import & export
 # 2. export issues, extract zip file and copy db-2.0.json to JSON_DIR (defaults to <repo>/tmp)
 # 3. set report period with start_date / end_date
 JSON_DIR = pathlib.Path(__file__).parents[1] / 'tmp'
-start_date = datetime.date(2021, 3, 15)
-end_date = datetime.date(2021, 11, 1)
-
-
+start_date = datetime.date(2021, 7, 1)
+end_date = datetime.date(2021, 12, 31)
 
 PATH_DB_JSON = JSON_DIR / 'db-2.0.json'
-PATH_REPORT = JSON_DIR / 'issue_report.csv'
+PATH_CSV_REPORT = JSON_DIR / f'issue_report_{start_date}_{end_date}.csv'
 assert PATH_DB_JSON.is_file(), 'No db-2.0.json, no stats!'
 assert start_date < end_date
+
+def csv2xlsx(path_csv):
+    path_csv = pathlib.Path(path_csv)
+    from xlsxwriter.workbook import Workbook
+    path_xlsx = path_csv.parent / f'{os.path.splitext(path_csv.name)[0]}.xlsx'
+    workbook = Workbook(path_xlsx)
+    # float_format = workbook.add_format({'num_format': ''})
+    worksheet = workbook.add_worksheet()
+    rxIsInt = re.compile(r'^\d+$')
+    rxIsFloat = re.compile(r'^\d+([.,]\d*)?$')
+    with open(path_csv, 'rt', encoding='utf8') as f:
+        reader = csv.reader(f)
+        for r, row in enumerate(reader):
+            for c, col in enumerate(row):
+                if rxIsInt.match(col):
+                    col = int(col)
+                elif rxIsFloat.match(col):
+                    col = float(col)
+                worksheet.write(r, c, col)
+    workbook.close()
+
+
 with open(PATH_DB_JSON, 'r', encoding='utf-8') as f:
     DB = json.load(f)
 
+# DS = pd.read_json(PATH_DB_JSON.as_posix())
 ISSUES = DB['issues']
 
 CREATED_ISSUES = [i for i in ISSUES if start_date
@@ -51,7 +81,7 @@ print(f'Updated: {len(UPDATED_ISSUES)}')
 for k in sorted(UPDATED_BY_STATUS.keys()):
     print(f'\t{k}: {len(UPDATED_BY_STATUS[k])}')
 
-with open(PATH_REPORT, 'w', encoding='utf-8', newline='') as f:
+with open(PATH_CSV_REPORT, 'w', encoding='utf-8', newline='') as f:
     states = ['new', 'open', 'on hold', 'resolved', 'closed', 'duplicate', 'wontfix', 'invalid']
     fieldnames = ['action', 'total'] + states
     writer = csv.DictWriter(f, fieldnames=fieldnames)
@@ -70,3 +100,4 @@ with open(PATH_REPORT, 'w', encoding='utf-8', newline='') as f:
     writer.writerow(ROW1)
     writer.writerow(ROW2)
 
+csv2xlsx(PATH_CSV_REPORT)
--- snippets/lesterfixes.py
+++
@@ -1,7 +0,0 @@
-from qgis.PyQt.QtWidgets import QAction
-
-a = QAction('info')
-b = QAction('info', None)
-
-print(a.text())
-print(b.text())
