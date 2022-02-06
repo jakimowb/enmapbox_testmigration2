57051da0cc0fbe30c80556b4b9791d85851184ee
janzandr <andreas.rabe@geo.hu-berlin.de>
2022-02-06 01:02:54 +0100

    resolves #977

--- enmapboxprocessing/algorithm/aggregaterasterbandsalgorithm.py
+++ enmapboxprocessing/algorithm/aggregaterasterbandsalgorithm.py
@@ -1,17 +1,14 @@
-from collections import OrderedDict
-from math import ceil
+from math import ceil, nan, inf
 from typing import Dict, Any, List, Tuple
 
 import numpy as np
 from osgeo import gdal
-from qgis._core import (QgsProcessingContext, QgsProcessingFeedback, QgsRasterLayer,
-                        Qgis, QgsProcessingException, QgsMapLayer)
+from qgis._core import (QgsProcessingContext, QgsProcessingFeedback, Qgis)
 
-from enmapboxprocessing.algorithm.layertomaskalgorithm import LayerToMaskAlgorithm
 from enmapboxprocessing.driver import Driver
 from enmapboxprocessing.enmapalgorithm import EnMAPProcessingAlgorithm, Group
+from enmapboxprocessing.numpyutils import NumpyUtils
 from enmapboxprocessing.rasterreader import RasterReader
-from enmapboxprocessing.typing import ClassifierDump
 from enmapboxprocessing.utils import Utils
 from typeguard import typechecked
 
@@ -19,41 +16,32 @@ from typeguard import typechecked
 @typechecked
 class AggregateRasterBandsAlgorithm(EnMAPProcessingAlgorithm):
     P_RASTER, _RASTER = 'raster', 'Raster layer'
-    P_FUNCTION, _FUNCTION = 'function', 'Aggregation function'
-    FUNCTIONS = OrderedDict(
-        mean=np.mean,
-        std=np.std,
-        min=np.min,
-        p5=lambda array, axis: np.percentile(array, 5, axis),
-        p25=lambda array, axis: np.percentile(array, 55, axis),
-        median=np.median,
-        p75=lambda array, axis: np.percentile(array, 75, axis),
-        p95=lambda array, axis: np.percentile(array, 95, axis),
-        max=np.max,
-        sum=np.sum,
-        product=np.product,
-        range=np.ptp,  # The name of the function comes from the acronym for ‘peak to peak’.
-        iqr=lambda array, axis: np.subtract(*np.percentile(array, [75, 25], axis)),
-        any=np.any,
-        all=np.all
-    )
-    (
-        MeanFunction, StdFunction, MinFunction, P5Function, P25Function, MedianFunction, P75Function, P95Function,
-        MaxFunction, SumFunction, ProductFunction, RangeFunction, IqrFunction, AnyFunction, AllFunction
-    ) = range(len(FUNCTIONS))
-    O_FUNCTION = FUNCTIONS.keys()
+    P_FUNCTION, _FUNCTION = 'function', 'Aggregation functions'
     P_OUTPUT_RASTER, _OUTPUT_RASTER = 'outputRaster', 'Output raster layer'
+
+    O_FUNCTION = [
+        'arithmetic mean', 'standard deviation', 'variance', 'minimum', 'median', 'maximum', 'sum', 'product',
+        'range', 'interquartile range', 'any true', 'all true', 'arg minimum', 'arg maximum'
+    ]
+    (
+        ArithmeticMeanFunction, StandardDeviationFunction, VarianceFunction, MinimumFunction, MedianFunction,
+        MaximumFunction, SumFunction, ProductFunction, RangeFunction, InterquartileRangeFunction, AnyTrueFunction,
+        AllTrueFunction, ArgMinimumFunction, ArgMaximumFunction
+    ) = range(len(O_FUNCTION))
+    P0 = len(O_FUNCTION)
+    O_FUNCTION.extend([f'{i}-th percentile' for i in range(101)])
 
     def displayName(self) -> str:
         return 'Aggregate raster layer bands'
 
     def shortDescription(self) -> str:
-        return 'Aggregate raster layer bands.'
+        return 'Compute various aggregation functions over all bands, while ignoring no data values.'
 
     def helpParameters(self) -> List[Tuple[str, str]]:
         return [
             (self._RASTER, 'A raster layer with bands to be aggregated.'),
-            (self._FUNCTION, 'Functions to be used. Number of selected functions equals number of output bands.'),
+            (self._FUNCTION, 'Functions to be used. '
+                             'Number and order of selected functions equals number and order of output bands.'),
             (self._OUTPUT_RASTER, self.RasterFileDestination)
         ]
 
@@ -62,41 +50,98 @@ class AggregateRasterBandsAlgorithm(EnMAPProcessingAlgorithm):
 
     def initAlgorithm(self, configuration: Dict[str, Any] = None):
         self.addParameterRasterLayer(self.P_RASTER, self._RASTER)
-        self.addParameterEnum(self.P_FUNCTION, self._FUNCTION, self.O_FUNCTION, True, [self.MeanFunction])
+        self.addParameterEnum(self.P_FUNCTION, self._FUNCTION, self.O_FUNCTION, True, None)
         self.addParameterRasterDestination(self.P_OUTPUT_RASTER, self._OUTPUT_RASTER)
 
     def processAlgorithm(
             self, parameters: Dict[str, Any], context: QgsProcessingContext, feedback: QgsProcessingFeedback
     ) -> Dict[str, Any]:
         raster = self.parameterAsRasterLayer(parameters, self.P_RASTER, context)
-        functions = self.parameterAsEnums(parameters, self.P_FUNCTION, context)
+        functionIndices = self.parameterAsEnums(parameters, self.P_FUNCTION, context)
         format, options = self.GTiffFormat, self.DefaultGTiffCreationOptions
         filename = self.parameterAsOutputLayer(parameters, self.P_OUTPUT_RASTER, context)
-        maximumMemoryUsage = gdal.GetCacheMax()
 
         with open(filename + '.log', 'w') as logfile:
             feedback, feedback2 = self.createLoggingFeedback(feedback, logfile)
             self.tic(feedback, parameters, context)
 
             reader = RasterReader(raster)
-            writer = Driver(filename, format, options, feedback).createLike(reader, Qgis.Float32, len(functions))
+            bandCount = len(functionIndices)
+            writer = Driver(filename, format, options, feedback).createLike(reader, Qgis.Float32, bandCount)
             noDataValue = Utils.defaultNoDataValue(np.float32)
-            lineMemoryUsage = reader.lineMemoryUsage(reader.bandCount() + len(functions), 4)
-            blockSizeY = min(raster.height(), ceil(maximumMemoryUsage / lineMemoryUsage))
+            lineMemoryUsage = reader.lineMemoryUsage(reader.bandCount() + bandCount, 4)
+            blockSizeY = min(raster.height(), ceil(gdal.GetCacheMax() / lineMemoryUsage))
             blockSizeX = raster.width()
             for block in reader.walkGrid(blockSizeX, blockSizeY, feedback):
                 array = np.array(reader.arrayFromBlock(block), dtype=np.float32)
-                invalid = ~np.all(reader.maskArray(array), axis=0)
-                for bandNo, fi in enumerate(functions, 1):
-                    funcName, func = list(self.FUNCTIONS.items())[fi]
-                    outarray = func(array, axis=0)
-                    outarray = np.array(outarray, dtype=np.float32)
-                    outarray[invalid] = noDataValue
+                mask = reader.maskArray(array)
+                array[np.logical_not(mask)] = nan
+
+                if (self.AnyTrueFunction in functionIndices) or self.AllTrueFunction in functionIndices:
+                    arrayAsBool = np.logical_and(np.isfinite(array), array)  # take care of NaN and Inf values
+
+                # Calculate all percentiles at once.
+                q = [i - self.P0 for i in functionIndices if i >= self.P0]
+                for i, outarray in enumerate(NumpyUtils.nanpercentile(array, q)):
+                    bandNo = functionIndices.index(q[i] + self.P0) + 1
+                    outarray[np.isnan(outarray)] = noDataValue
                     writer.writeArray2d(outarray, bandNo, xOffset=block.xOffset, yOffset=block.yOffset)
 
-            for bandNo, fi in enumerate(functions, 1):
-                funcName, func = list(self.FUNCTIONS.items())[fi]
-                writer.setBandName(funcName, bandNo)
+                # Calculate all other indices individually.
+                for bandNo, functionIndex in enumerate(functionIndices, 1):
+                    if functionIndex >= self.P0:  # skip percentiles
+                        continue
+                    elif functionIndex == self.ArithmeticMeanFunction:
+                        outarray = np.nanmean(array, axis=0)
+                    elif functionIndex == self.StandardDeviationFunction:
+                        outarray = np.nanstd(array, axis=0)
+                    elif functionIndex == self.VarianceFunction:
+                        outarray = np.nanvar(array, axis=0)
+                    elif functionIndex == self.MinimumFunction:
+                        outarray = np.nanmin(array, axis=0)
+                    elif functionIndex == self.MedianFunction:
+                        outarray = np.nanmedian(array, axis=0)
+                    elif functionIndex == self.MaximumFunction:
+                        outarray = np.nanmax(array, axis=0)
+                    elif functionIndex == self.SumFunction:
+                        outarray = np.nansum(array, axis=0)
+                    elif functionIndex == self.ProductFunction:
+                        outarray = np.nanprod(array, axis=0)
+                    elif functionIndex == self.RangeFunction:
+                        outarray = np.nanmax(array, axis=0) - np.nanmin(array, axis=0)
+                    elif functionIndex == self.InterquartileRangeFunction:
+                        p25, p75 = NumpyUtils.nanpercentile(array, [25, 75])
+                        outarray = p75 - p25
+                    elif functionIndex == self.AnyTrueFunction:
+                        outarray = np.any(arrayAsBool, axis=0).astype(np.float32)
+                    elif functionIndex == self.AllTrueFunction:
+                        outarray = np.all(arrayAsBool, axis=0).astype(np.float32)
+                    elif functionIndex == self.ArgMinimumFunction:
+                        # Numpy nanargmin will throw an All-NaN slice encountered ValueError, instead of just a warning.
+                        # Append a inf band to prevent this.
+                        array2 = list(array)
+                        array2.append(np.full_like(array[0], inf))
+                        outarray = np.nanargmin(array2, axis=0).astype(np.float32)
+                        # Now, set All-NaN slice pixel to nan.
+                        outarray[outarray == len(array)] = nan
+                    elif functionIndex == self.ArgMaximumFunction:
+                        # Numpy nanargmax will throw an All-NaN slice encountered ValueError, instead of just a warning.
+                        # Append a -inf band to prevent this.
+                        array2 = list(array)
+                        array2.append(np.full_like(array[0], -inf))
+                        outarray = np.nanargmax(array2, axis=0).astype(np.float32)
+                        # Now, set All-NaN slice pixel to nan.
+                        outarray[outarray == len(array)] = nan
+                    else:
+                        raise ValueError()
+
+                    assert outarray.dtype == np.float32, self.O_FUNCTION[functionIndex]
+                    outarray[np.isnan(outarray)] = noDataValue
+                    writer.writeArray2d(outarray, bandNo, xOffset=block.xOffset, yOffset=block.yOffset)
+
+            for bandNo, functionIndex in enumerate(functionIndices, 1):
+                bandName = self.O_FUNCTION[functionIndex]
+                writer.setBandName(bandName, bandNo)
 
             writer.setNoDataValue(noDataValue)
 
