e7f04731dd133e1f266f0530af7788738827556f
janzandr <andreas.rabe@geo.hu-berlin.de>
2022-02-08 23:08:30 +0100

    added context manager for blocking QObject signals

--- enmapbox/utils.py
+++ enmapbox/utils.py
@@ -1,8 +1,9 @@
 from typing import Optional
 
-from qgis.core import QgsRasterLayer
+from PyQt5.QtCore import QObject
 
 from enmapboxprocessing.algorithm.createspectralindicesalgorithm import CreateSpectralIndicesAlgorithm
+from qgis.core import QgsRasterLayer
 from typeguard import typechecked
 
 
@@ -14,3 +15,20 @@ def findBroadBand(raster: QgsRasterLayer, name: str, strict=False) -> Optional[i
     """
 
     return CreateSpectralIndicesAlgorithm.findBroadBand(raster, name, strict)
+
+
+@typechecked
+class BlockSignals(object):
+    """Context manager for blocking QObject signals."""
+
+    def __init__(self, *objects: QObject):
+        self.objects = objects
+
+    def __enter__(self):
+        self.signalsBlocked = [obj.signalsBlocked() for obj in self.objects]
+        for object in self.objects:
+            object.blockSignals(True)
+
+    def __exit__(self, exc_type, exc_value, tb):
+        for object, signalsBlocked in zip(self.objects, self.signalsBlocked):
+            object.blockSignals(signalsBlocked)
